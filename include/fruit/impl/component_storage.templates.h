google/fruit

commit b35bcb4bf69ce9a707af4663a8b949bacf16591b

Change the representation of a BindingData, now occupies 2 pointers instead of 4.

include/fruit/impl/component_storage.templates.h
@@ -139,32 +139,6 @@ inline void ComponentStorage::check(bool b, MessageGenerator messageGenerator) {
 }
 
 template <typename C>
-inline void ComponentStorage::createBindingData(void* (*create)(InjectorStorage&, void*),
-                                             void* createArgument,
-                                             void (*deleteOperation)(void*)) {
-  createBindingData(getTypeInfo<C>(), create, createArgument, deleteOperation);
-}
-
-template <typename C>
-inline void ComponentStorage::createBindingData(void* instance,
-                                                void (*destroy)(void*)) {
-  createBindingData(getTypeInfo<C>(), instance, destroy);
-}
-
-template <typename C>
-inline void ComponentStorage::createBindingDataForMultibinding(void* (*create)(InjectorStorage&, void*),
-                                                               void* createArgument,
-                                                               void (*deleteOperation)(void*)) {
-  createBindingDataForMultibinding(getTypeInfo<C>(), create, createArgument, deleteOperation, createSingletonSet<C>);
-}
-
-template <typename C>
-inline void ComponentStorage::createBindingDataForMultibinding(void* instance,
-                                                               void (*destroy)(void*)) {
-  createBindingDataForMultibinding(getTypeInfo<C>(), instance, destroy, createSingletonSet<C>);
-}
-
-template <typename C>
 inline std::shared_ptr<char> ComponentStorage::createSingletonSet(InjectorStorage& storage) {
   const TypeInfo* typeInfo = getTypeInfo<C>();
   auto itr = storage.typeRegistryForMultibindings.find(typeInfo);
@@ -183,7 +157,7 @@ inline std::shared_ptr<char> ComponentStorage::createSingletonSet(InjectorStorag
   
   std::set<C*> s;
   for (const BindingData& bindingData : itr->second.bindingDatas) {
-    s.insert(reinterpret_cast<C*>(bindingData.storedSingleton));
+    s.insert(reinterpret_cast<C*>(bindingData.getStoredSingleton()));
   }
   
   std::shared_ptr<std::set<C*>> sPtr = std::make_shared<std::set<C*>>(std::move(s));
@@ -204,14 +178,14 @@ inline void ComponentStorage::bind() {
     // This step is needed when the cast C->I changes the pointer
     // (e.g. for multiple inheritance).
     I* iPtr = static_cast<I*>(cPtr);
-    return reinterpret_cast<void*>(iPtr);
+    return std::make_pair(reinterpret_cast<void*>(iPtr), nopDeleter);
   };
-  createBindingData<I>(create, nullptr, nopDeleter);
+  createBindingData(getTypeInfo<I>(), create, nullptr);
 }
 
 template <typename C>
 inline void ComponentStorage::bindInstance(C& instance) {
-  createBindingData<C>(&instance, nopDeleter);
+  createBindingData(getTypeInfo<C>(), &instance, nopDeleter);
 }
 
 template <typename C, typename... Args>
@@ -222,13 +196,13 @@ inline void ComponentStorage::registerProvider(C* (*provider)(Args...)) {
   auto create = [](InjectorStorage& m, void* arg) {
     provider_type provider = reinterpret_cast<provider_type>(arg);
     C* cPtr = provider(m.get<Args>()...);
-    return reinterpret_cast<void*>(cPtr);
+    auto destroy = [](void* p) {
+      C* cPtr = reinterpret_cast<C*>(p);
+      delete cPtr;
+    };
+    return std::make_pair(reinterpret_cast<void*>(cPtr), static_cast<void(*)(void*)>(destroy));
   };
-  auto destroy = [](void* p) {
-    C* cPtr = reinterpret_cast<C*>(p);
-    delete cPtr;
-  };
-  createBindingData<C>(create, reinterpret_cast<void*>(provider), destroy);
+  createBindingData(getTypeInfo<C>(), create, reinterpret_cast<void*>(provider));
 }
 
 template <typename C, bool is_movable, typename... Args>
@@ -244,13 +218,13 @@ inline void ComponentStorage::registerProvider(C (*provider)(Args...)) {
   auto create = [](InjectorStorage& m, void* arg) {
     provider_type provider = reinterpret_cast<provider_type>(arg);
     C* cPtr = m.constructSingleton<C, Args...>(provider(m.get<Args>()...));
-    return reinterpret_cast<void*>(cPtr);
+    auto destroy = [](void* p) {
+      C* cPtr = reinterpret_cast<C*>(p);
+      cPtr->C::~C();
+    };
+    return std::make_pair(reinterpret_cast<void*>(cPtr), static_cast<void(*)(void*)>(destroy));
   };
-  auto destroy = [](void* p) {
-    C* cPtr = reinterpret_cast<C*>(p);
-    cPtr->C::~C();
-  };
-  createBindingData<C>(create, reinterpret_cast<void*>(provider), destroy);
+  createBindingData(getTypeInfo<C>(), create, reinterpret_cast<void*>(provider));
 }
 
 template <typename C, typename... Args>
@@ -259,13 +233,13 @@ inline void ComponentStorage::registerConstructor() {
   auto create = [](InjectorStorage& m, void* arg) {
     (void)arg;
     C* cPtr = m.constructSingleton<C, Args...>(m.get<Args>()...);
-    return reinterpret_cast<void*>(cPtr);
+    auto destroy = [](void* p) {
+      C* cPtr = reinterpret_cast<C*>(p);
+      cPtr->C::~C();
+    };
+    return std::make_pair(reinterpret_cast<void*>(cPtr), static_cast<void(*)(void*)>(destroy));
   };
-  auto destroy = [](void* p) {
-    C* cPtr = reinterpret_cast<C*>(p);
-    cPtr->C::~C();
-  };
-  createBindingData<C>(create, nullptr, destroy);
+  createBindingData(getTypeInfo<C>(), create, nullptr);
 }
 
 // I, C must not be pointers.
@@ -278,14 +252,14 @@ inline void ComponentStorage::addMultibinding() {
     // This step is needed when the cast C->I changes the pointer
     // (e.g. for multiple inheritance).
     I* iPtr = static_cast<I*>(cPtr);
-    return reinterpret_cast<void*>(iPtr);
+    return std::make_pair(reinterpret_cast<void*>(iPtr), nopDeleter);
   };
-  createBindingDataForMultibinding<I>(create, nullptr, nopDeleter);
+  createBindingDataForMultibinding(getTypeInfo<I>(), create, nullptr, createSingletonSet<I>);
 }
 
 template <typename C>
 inline void ComponentStorage::addInstanceMultibinding(C& instance) {
-  createBindingDataForMultibinding<C>(&instance, nopDeleter);
+  createBindingDataForMultibinding(getTypeInfo<C>(), &instance, nopDeleter, createSingletonSet<C>);
 }
 
 template <typename C, typename... Args>
@@ -296,13 +270,13 @@ inline void ComponentStorage::registerMultibindingProvider(C* (*provider)(Args..
   auto create = [](InjectorStorage& m, void* arg) {
     provider_type provider = reinterpret_cast<provider_type>(arg);
     C* cPtr = provider(m.get<std::forward<Args>>()...);
-    return reinterpret_cast<void*>(cPtr);
+    auto destroy = [](void* p) {
+      C* cPtr = reinterpret_cast<C*>(p);
+      delete cPtr;
+    };
+    return std::make_pair(reinterpret_cast<void*>(cPtr), static_cast<void(*)(void*)>(destroy));
   };
-  auto destroy = [](void* p) {
-    C* cPtr = reinterpret_cast<C*>(p);
-    delete cPtr;
-  };
-  createBindingDataForMultibinding<C>(create, reinterpret_cast<void*>(provider), destroy);
+  createBindingDataForMultibinding(getTypeInfo<C>(), create, reinterpret_cast<void*>(provider), createSingletonSet<C>);
 }
 
 template <typename C, typename... Args>
@@ -315,13 +289,13 @@ inline void ComponentStorage::registerMultibindingProvider(C (*provider)(Args...
   auto create = [](InjectorStorage& m, void* arg) {
     provider_type provider = reinterpret_cast<provider_type>(arg);
     C* cPtr = m.constructSingleton<C, Args...>(provider(m.get<Args>()...));
-    return reinterpret_cast<void*>(cPtr);
+    auto destroy = [](void* p) {
+      C* cPtr = reinterpret_cast<C*>(p);
+      cPtr->C::~C();
+    };
+    return std::make_pair(reinterpret_cast<void*>(cPtr), static_cast<void(*)(void*)>(destroy));
   };
-  auto destroy = [](void* p) {
-    C* cPtr = reinterpret_cast<C*>(p);
-    cPtr->C::~C();
-  };
-  createBindingDataForMultibinding<C>(create, reinterpret_cast<void*>(provider), destroy);
+  createBindingDataForMultibinding(getTypeInfo<C>(), create, reinterpret_cast<void*>(provider), createSingletonSet<C>);
 }
 
 template <typename AnnotatedSignature, typename... Argz>
@@ -333,9 +307,10 @@ inline void ComponentStorage::registerFactory(SignatureType<AnnotatedSignature>(
     Signature* factory = reinterpret_cast<Signature*>(arg);
     std::function<InjectedFunctionType>* fPtr = 
         new std::function<InjectedFunctionType>(BindAssistedFactoryForValue<AnnotatedSignature>(m, factory));
-    return reinterpret_cast<void*>(fPtr);
+    auto destroy = standardDeleter<std::function<InjectedFunctionType>>;
+    return std::make_pair(reinterpret_cast<void*>(fPtr), destroy);
   };
-  createBindingData<std::function<InjectedFunctionType>>(create, reinterpret_cast<void*>(factory), standardDeleter<std::function<InjectedFunctionType>>);
+  createBindingData(getTypeInfo<std::function<InjectedFunctionType>>(), create, reinterpret_cast<void*>(factory));
 }
 
 } // namespace fruit

include/fruit/impl/component_storage.h
@@ -39,7 +39,6 @@ struct BindAssistedFactory;
  */
 class ComponentStorage {
 private:
-    
   using BindingData = InjectorStorage::BindingData;
   using BindingDataForMultibinding = InjectorStorage::BindingDataForMultibinding;
   using BindingDataSetForMultibinding = InjectorStorage::BindingDataSetForMultibinding;
@@ -62,42 +61,22 @@ private:
   void printError(const std::string& message);
   
   void createBindingData(const TypeInfo* typeInfo,
-                         void* (*create)(InjectorStorage&, void*), 
-                         void* createArgument,
-                         void (*deleteOperation)(void*));
+                         BindingData::create_t create, 
+                         BindingData::createArgument_t createArgument);
   
   void createBindingData(const TypeInfo* typeInfo,
-                         void* storedSingleton,
-                         void (*deleteOperation)(void*));
+                         BindingData::object_t storedSingleton,
+                         BindingData::destroy_t deleteOperation);
   
   void createBindingDataForMultibinding(const TypeInfo* typeInfo,
-                                        void* (*create)(InjectorStorage&, void*),
-                                        void* createArgument,
-                                        void (*deleteOperation)(void*),
+                                        BindingData::create_t create,
+                                        BindingData::createArgument_t createArgument,
                                         std::shared_ptr<char>(*createSet)(InjectorStorage&));
   
   void createBindingDataForMultibinding(const TypeInfo* typeInfo,
-                                        void* storedSingleton,
-                                        void (*deleteOperation)(void*),
+                                        BindingData::object_t storedSingleton,
+                                        BindingData::destroy_t deleteOperation,
                                         std::shared_ptr<char>(*createSet)(InjectorStorage&));
-  
-  template <typename C>
-  void createBindingData(void* (*create)(InjectorStorage&, void*),
-                         void* createArgument,
-                         void (*deleteOperation)(void*));
-  
-  template <typename C>
-  void createBindingDataForMultibinding(void* (*create)(InjectorStorage&, void*),
-                                        void* createArgument,
-                                        void (*deleteOperation)(void*));
-  
-  template <typename C>
-  void createBindingData(void* storedSingleton,
-                         void (*deleteOperation)(void*));
-  
-  template <typename C>
-  void createBindingDataForMultibinding(void* storedSingleton,
-                                        void (*deleteOperation)(void*));
   
   template <typename C>
   static std::shared_ptr<char> createSingletonSet(InjectorStorage& storage);

include/fruit/impl/injector_storage.h
@@ -40,21 +40,59 @@ struct GetHelper;
  */
 class InjectorStorage {
 public:
-  struct BindingData {
-    // The stored singleton (if it was already created) or nullptr.
-    // Stores a casted T*
-    void* storedSingleton;
+  class BindingData {
+  public:
+    using createArgument_t = void*;
+    using object_t = void*;
+    using destroy_t = void(*)(object_t);
+    using create_t = std::pair<object_t, destroy_t>(*)(InjectorStorage&, createArgument_t);
     
-    // A function pointer.
-    // This is NULL if the type wasn't yet bound or if an instance was bound (so storedSingleton!=nullptr).
-    void* (*create)(InjectorStorage&, void*);
+  private:
+    // The low-order bit of p1 stores an is_created bool.
+    // 
+    // All the other bits store either:
+    //     
+    // * create, of type create_t if is_created==false
+    //   A function pointer.
+    //   This is NULL if the type wasn't yet bound or if an instance was bound (so storedSingleton!=nullptr).
+    //   The return type is a pair (constructedObject, destroyOperation).
+    // 
+    // * destroy, of type destroy_t if is_created==true
+    //   The operation to destroy this singleton, or a no-op if it shouldn't be.
+    void* p1;
     
-    // This is passed to create() when creating the singleton.
-    // There are no guarantees on the value, it might even be nullptr.
-    void* createArgument;
+    // This stores either:
+    // 
+    // * createArgument, of type createArgument_t if is_created==false
+    //   This is passed to create() when creating the singleton.
+    //   There are no guarantees on the value, it might even be nullptr.
+    // 
+    // * storedSingleton, of type object_t if is_created==true
+    //   The stored singleton (if it was already created) or nullptr.
+    //   Stores a casted T*.
+    void* p2;
     
-    // The operation to destroy this singleton, or a no-op if it shouldn't be.
-    void (*destroy)(void*);
+  public:
+    BindingData() = default;
+    
+    // Binding data for a singleton not already constructed.
+    BindingData(create_t create, createArgument_t createArgument);
+      
+    // Binding data for a singleton already constructed.
+    BindingData(destroy_t destroy, object_t object);
+    
+    bool isCreated() const;
+    
+    // These assume !isCreated().
+    create_t getCreate() const;
+    createArgument_t getCreateArgument() const;
+    
+    // These assume isCreated().
+    destroy_t getDestroy() const;
+    object_t getStoredSingleton() const;
+    
+    // Assumes !isCreated(). After this call, isCreated()==true.
+    void create(InjectorStorage& storage);
     
     bool operator==(const BindingData& other) const;
     

include/fruit/impl/injector_storage.inlines.h
@@ -23,6 +23,54 @@
 namespace fruit {
 namespace impl {
 
+inline InjectorStorage::BindingData::BindingData(create_t create, createArgument_t createArgument)
+: p1(reinterpret_cast<void*>(create)), p2(reinterpret_cast<void*>(createArgument)) {
+}
+  
+inline InjectorStorage::BindingData::BindingData(destroy_t destroy, object_t object) 
+: p1(reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(destroy) ^ 1)), p2(reinterpret_cast<void*>(object)) {
+}
+
+inline bool InjectorStorage::BindingData::isCreated() const {
+  return reinterpret_cast<uintptr_t>(p1) & 1;
+}
+
+inline InjectorStorage::BindingData::create_t InjectorStorage::BindingData::getCreate() const {
+  return reinterpret_cast<create_t>(p1);
+}
+
+inline InjectorStorage::BindingData::createArgument_t InjectorStorage::BindingData::getCreateArgument() const {
+  return reinterpret_cast<createArgument_t>(p2);
+}
+
+inline InjectorStorage::BindingData::destroy_t InjectorStorage::BindingData::getDestroy() const {
+  return reinterpret_cast<destroy_t>(reinterpret_cast<uintptr_t>(p1) ^ 1);
+}
+
+inline InjectorStorage::BindingData::object_t InjectorStorage::BindingData::getStoredSingleton() const {
+  return reinterpret_cast<object_t>(p2);
+}
+
+inline void InjectorStorage::BindingData::create(InjectorStorage& storage) {
+  destroy_t destroyOp;
+  object_t obj;
+  std::tie(obj, destroyOp) = getCreate()(storage, getCreateArgument());
+  (*this) = BindingData(destroyOp, obj);
+}
+
+inline bool InjectorStorage::BindingData::operator==(const BindingData& other) const {
+  return std::tie(p1, p2)
+      == std::tie(other.p1, other.p2);
+}
+
+inline bool InjectorStorage::BindingData::operator<(const BindingData& other) const {
+  // `destroy' is intentionally not compared.
+  // If the others are equal it should also be equal. If it isn't, the two BindingData structs
+  // are still equivalent because they produce the same injected object.
+  return std::tie(p1, p2)
+       < std::tie(other.p1, other.p2);
+}
+
 inline void InjectorStorage::check(bool b, const char* message) {
   check(b, [=](){return std::string(message);});
 }

src/component_storage.cpp
@@ -29,35 +29,8 @@
 using std::cout;
 using std::endl;
 
-namespace {
-  
-std::string multipleBindingsError(const fruit::impl::TypeInfo* typeInfo) {
-  return "the type " + typeInfo->name() + " was provided more than once, with different bindings.\n"
-        + "This was not caught at compile time because at least one of the involved components bound this type but didn't expose it in the component signature.\n"
-        + "If the type has a default constructor or an Inject annotation, this problem may arise even if this type is bound/provided by only one component (and then hidden), if this type is auto-injected in another component.\n"
-        + "If the source of the problem is unclear, try exposing this type in all the component signatures where it's bound; if no component hides it this can't happen.\n";
-}
-
-} // namespace
-
 namespace fruit {
 namespace impl {
-
-bool ComponentStorage::BindingData::operator==(const BindingData& other) const {
-  // `destroy' is intentionally not compared.
-  // If the others are equal it should also be equal. If it isn't, the two BindingData structs
-  // are still equivalent because they produce the same injected object.
-  return std::tie(      storedSingleton,       create,       createArgument)
-      == std::tie(other.storedSingleton, other.create, other.createArgument);
-}
-
-bool ComponentStorage::BindingData::operator<(const BindingData& other) const {
-  // `destroy' is intentionally not compared.
-  // If the others are equal it should also be equal. If it isn't, the two BindingData structs
-  // are still equivalent because they produce the same injected object.
-  return std::tie(      storedSingleton,       create,       createArgument)
-       < std::tie(other.storedSingleton, other.create, other.createArgument);
-}
 
 void ComponentStorage::install(ComponentStorage other) {
   // Heuristic to try saving an allocation by appending to the largest vector.
@@ -78,64 +51,40 @@ void ComponentStorage::install(ComponentStorage other) {
 }
 
 void ComponentStorage::createBindingData(const TypeInfo* typeInfo,
-                                         void* (*create)(InjectorStorage&, void*),
-                                         void* createArgument,
-                                         void (*destroy)(void*)) {
+                                         BindingData::create_t create,
+                                         BindingData::createArgument_t createArgument) {
 #ifdef FRUIT_EXTRA_DEBUG
   std::cerr << "In ComponentStorage::createBindingData for type " << typeInfo->name() << std::endl;
 #endif
-  BindingData bindingData;
-  bindingData.create = create;
-  bindingData.createArgument = createArgument;
-  bindingData.storedSingleton = nullptr;
-  bindingData.destroy = destroy;
-  typeRegistry.emplace_back(typeInfo, bindingData);
+  typeRegistry.emplace_back(typeInfo, BindingData(create, createArgument));
 }
 
 void ComponentStorage::createBindingData(const TypeInfo* typeInfo,
-                                         void* storedSingleton,
-                                         void (*destroy)(void*)) {
+                                         BindingData::object_t storedSingleton,
+                                         BindingData::destroy_t destroy) {
 #ifdef FRUIT_EXTRA_DEBUG
   std::cerr << "In ComponentStorage::createBindingData for type " << typeInfo->name() << std::endl;
 #endif
-  BindingData bindingData;
-  bindingData.storedSingleton = storedSingleton;
-  bindingData.create = nullptr;
-  bindingData.createArgument = nullptr;
-  bindingData.destroy = destroy;
-  typeRegistry.emplace_back(typeInfo, bindingData);
+  typeRegistry.emplace_back(typeInfo, BindingData(destroy, storedSingleton));
 }
 
 void ComponentStorage::createBindingDataForMultibinding(const TypeInfo* typeInfo,
-                                                     void* (*create)(InjectorStorage&, void*),
-                                                     void* createArgument,
-                                                     void (*destroy)(void*),
-                                                     std::shared_ptr<char>(*createSet)(InjectorStorage&)) {
-  BindingData bindingData;
-  bindingData.create = create;
-  bindingData.createArgument = createArgument;
-  bindingData.storedSingleton = nullptr;
-  bindingData.destroy = destroy;
-  
+                                                        BindingData::create_t create,
+                                                        BindingData::createArgument_t createArgument,
+                                                        std::shared_ptr<char>(*createSet)(InjectorStorage&)) {
   BindingDataForMultibinding bindingDataForMultibinding;
-  bindingDataForMultibinding.bindingData = bindingData;
+  bindingDataForMultibinding.bindingData = BindingData(create, createArgument);
   bindingDataForMultibinding.getSingletonSet = createSet;
   
   typeRegistryForMultibindings.emplace_back(typeInfo, bindingDataForMultibinding);
 }
 
 void ComponentStorage::createBindingDataForMultibinding(const TypeInfo* typeInfo,
-                                                     void* storedSingleton,
-                                                     void (*destroy)(void*),
-                                                     std::shared_ptr<char>(*createSet)(InjectorStorage&)) {
-  BindingData bindingData;
-  bindingData.storedSingleton = storedSingleton;
-  bindingData.create = nullptr;
-  bindingData.createArgument = nullptr;
-  bindingData.destroy = destroy;
-  
+                                                        BindingData::object_t storedSingleton,
+                                                        BindingData::destroy_t destroy,
+                                                        std::shared_ptr<char>(*createSet)(InjectorStorage&)) {
   BindingDataForMultibinding bindingDataForMultibinding;
-  bindingDataForMultibinding.bindingData = bindingData;
+  bindingDataForMultibinding.bindingData = BindingData(destroy, storedSingleton);
   bindingDataForMultibinding.getSingletonSet = createSet;
   
   typeRegistryForMultibindings.emplace_back(typeInfo, bindingDataForMultibinding);

src/injector_storage.cpp
@@ -43,11 +43,11 @@ namespace fruit {
 namespace impl {
 
 void InjectorStorage::ensureConstructed(const TypeInfo* typeInfo, BindingData& bindingData) {
-  if (bindingData.storedSingleton == nullptr) {
-    FruitCheck(bool(bindingData.create), [=](){return "attempting to create an instance for the type " + typeInfo->name() + " but there is no create operation";});
-    bindingData.storedSingleton = bindingData.create(*this, bindingData.createArgument);
+  if (!bindingData.isCreated()) {
+    bindingData.create(*this);
+    
     // This can happen if the user-supplied provider returns nullptr.
-    check(bindingData.storedSingleton != nullptr, [=](){return "attempting to get an instance for the type " + typeInfo->name() + " but got nullptr";});
+    check(bindingData.getStoredSingleton() != nullptr, [=](){return "attempting to get an instance for the type " + typeInfo->name() + " but got nullptr";});
     
     createdSingletons.push_back(typeInfo);
   }
@@ -58,11 +58,11 @@ void InjectorStorage::ensureConstructedMultibinding(const TypeInfo* typeInfo, Bi
   // We need to create a new set.
   std::set<BindingData> newBindingDatas;
   for (BindingData bindingData : bindingDataForMultibinding.bindingDatas) {
-    if (bindingData.storedSingleton == nullptr) {
-      FruitCheck(bool(bindingData.create), [=](){return "attempting to create an instance for the type " + typeInfo->name() + " but there is no create operation";});
-      bindingData.storedSingleton = bindingData.create(*this, bindingData.createArgument);
+    if (!bindingData.isCreated()) {
+      bindingData.create(*this);
+      
       // This can happen if the user-supplied provider returns nullptr.
-      check(bindingData.storedSingleton != nullptr, [=](){return "attempting to get an instance for the type " + typeInfo->name() + " but got nullptr";});
+      check(bindingData.getStoredSingleton() != nullptr, [=](){return "attempting to get an instance for the type " + typeInfo->name() + " but got nullptr";});
     }
     newBindingDatas.insert(bindingData);
   }
@@ -78,7 +78,7 @@ void* InjectorStorage::getPtr(const TypeInfo* typeInfo) {
     FruitCheck(false, [=](){return "attempting to getPtr() on a non-registered type: " + typeInfo->name();});
   }
   ensureConstructed(typeInfo, itr->second);
-  return itr->second.storedSingleton;
+  return itr->second.getStoredSingleton();
 }
 
 void InjectorStorage::printError(const std::string& message) {
@@ -101,8 +101,8 @@ void InjectorStorage::clear() {
   for (auto& elem : typeRegistryForMultibindings) {
     std::set<BindingData>& bindingDatas = elem.second.bindingDatas;
     for (const BindingData& bindingData : bindingDatas) {
-      if (bindingData.storedSingleton != nullptr) {
-        bindingData.destroy(bindingData.storedSingleton);
+      if (bindingData.isCreated()) {
+        bindingData.getDestroy()(bindingData.getStoredSingleton());
       }
     }
   }
@@ -112,8 +112,8 @@ void InjectorStorage::clear() {
     FruitCheck(itr != typeRegistry.end(), "internal error: attempting to destroy an non-registered type");
     BindingData& bindingData = itr->second;
     // Note: if this was a binding or user-provided object, the object is NOT destroyed.
-    if (bindingData.storedSingleton != nullptr) {
-      bindingData.destroy(bindingData.storedSingleton);
+    if (bindingData.isCreated()) {
+      bindingData.getDestroy()(bindingData.getStoredSingleton());
     }
   }
   createdSingletons.clear();

commit c000c2be8da3ba9c42528da5cd8ca42a0a02837c

Fix memory leak in registerFactory(), introduced in commit b35bcb4.

include/fruit/impl/component_storage.templates.h
@@ -305,14 +305,18 @@ inline void ComponentStorage::registerFactory(SignatureType<AnnotatedSignature>(
   check(factory != nullptr, "attempting to register nullptr as factory");
   using Signature = ConstructSignature<SignatureType<AnnotatedSignature>, RequiredArgsForAssistedFactory<AnnotatedSignature>>;
   using InjectedFunctionType = ConstructSignature<SignatureType<AnnotatedSignature>, InjectedFunctionArgsForAssistedFactory<AnnotatedSignature>>;
+  using fun_t = std::function<InjectedFunctionType>;
   auto create = [](InjectorStorage& m, void* arg) {
     Signature* factory = reinterpret_cast<Signature*>(arg);
-    std::function<InjectedFunctionType>* fPtr = 
-        new std::function<InjectedFunctionType>(BindAssistedFactoryForValue<AnnotatedSignature>(m, factory));
-    auto destroy = standardDeleter<std::function<InjectedFunctionType>>;
-    return std::make_pair(reinterpret_cast<void*>(fPtr), destroy);
+    fun_t* fPtr = 
+      m.constructSingleton<fun_t>(BindAssistedFactoryForValue<AnnotatedSignature>(m, factory));
+    auto destroy = [](void* p) {
+      fun_t* fPtr = reinterpret_cast<fun_t*>(p);
+      fPtr->~fun_t();
+    };
+    return std::make_pair(reinterpret_cast<void*>(fPtr), static_cast<void(*)(void*)>(destroy));
   };
-  createBindingData(getTypeInfo<std::function<InjectedFunctionType>>(), create, reinterpret_cast<void*>(factory));
+  createBindingData(getTypeInfo<fun_t>(), create, reinterpret_cast<void*>(factory));
 }
 
 } // namespace fruit