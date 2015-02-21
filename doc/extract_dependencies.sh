05e9b4c60acc6bc9181522862086ee2d4683ee7a
Marco Poletti <poletti.marco@gmail.com>
2015-02-21 18:26:41 +0000

    Improve the script and CMake targets for generating dependency graphs; now png are generated and the nodes have the filename as label.

--- doc/extract_dependencies.sh
+++ doc/extract_dependencies.sh
@@ -2,8 +2,9 @@
 
 echo "// Running from dir $PWD"
 echo 'digraph g {'
-for f in *.cpp *.h
+for f in $(ls *.cpp *.h)
 do
+  echo "$(echo "$f" | sed 's/\./_/g') [label=\"$f\"]"
   for g in $(fgrep -l "#include \"$f\"" *)
   do
     echo "$g -> $f" | sed 's/\./_/g'
