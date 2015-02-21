a9c04037e08d617beb17aa186b3307c3d299e158
Marco Poletti <poletti.marco@gmail.com>
2015-02-21 15:52:21 +0000

    Extract postsubmit script (executed using Travis CI) from .travis.yml, some improvements.

---
+++ postsubmit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+mkdir build
+cd build
+
+EXTRA_CMAKE_ARGS=()
+if [[ ${BUILD_TYPE} != "Debug" ]]
+then
+  EXTRA_CMAKE_ARGS+=(-DCMAKE_CXX_FLAGS="-O2 -W -Wall -Werror")
+fi
+
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${EXTRA_CMAKE_ARGS[@]}
+
+make -j2 VERBOSE=1
+
+cd tests
+if [[ ${BUILD_TYPE} == "Debug" ]]
+then
+  ctest --output-on-failure -j2
+else
+  # No need to execute the tests, but check that they still compile.
+  make -j2 VERBOSE=1
+fi
