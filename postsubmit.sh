007d1cb6551e99526683e5741f5debeb9231dc6b
Marco Poletti <poletti.marco@gmail.com>
2015-05-31 18:16:29 +0100

    Simplify the Travis CI config by moving the loop on build kind (debug/release) to the postsubmit script.

--- postsubmit.sh
+++ postsubmit.sh
@@ -9,30 +9,41 @@ run_make() {
 build_codebase() {
   cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCOMPILERS_TO_TEST=${CXX} -DCMAKE_CXX_FLAGS="${CXXFLAGS}"
   run_make
+  
   cd examples
   run_make
+  cd ..
+}
+
+run_tests() {
+  cd tests
+  run_make
+  ctest --output-on-failure -j2
   cd ..
 }
 
 mkdir build
 cd build
 
-if [[ ${BUILD_TYPE} == "Debug" ]]
-then
-  export CXXFLAGS="-O2          -W -Wall -Werror"
-else
-  export CXXFLAGS="-O2 -DNDEBUG -W -Wall -Werror"
-fi
+echo "==================================================="
+echo "=          Debug mode (compile + test)            ="
+echo "==================================================="
+export BUILD_TYPE="Debug"
+export CXXFLAGS="-O2 -W -Wall -Werror"
+build_codebase
+run_tests
 
+echo "==================================================="
+echo "=    Release mode with DNDEBUG (compile only)     ="
+echo "==================================================="
+export BUILD_TYPE="Release"
+export CXXFLAGS="-O2 -W -Wall -Werror -DNDEBUG"
 build_codebase
 
-if [[ ${BUILD_TYPE} != "Debug" ]]
-then
-  # Recompile everything without -DNDEBUG to prepare for test execution.
-  export CXXFLAGS="-O2 -W -Wall -Werror"
-  build_codebase
-fi
-
-cd tests
-run_make
-ctest --output-on-failure -j2
+echo "==================================================="
+echo "=  Release mode without DNDEBUG (compile + test)  ="
+echo "==================================================="
+export BUILD_TYPE="Release"
+export CXXFLAGS="-O2 -W -Wall -Werror"
+build_codebase
+run_tests
