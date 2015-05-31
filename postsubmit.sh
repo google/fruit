92be73d987ba3f6c82b1b301b3e9aa8a8f5adffd
Marco Poletti <poletti.marco@gmail.com>
2015-05-31 17:20:47 +0100

    Remove useless update-alternatives command from the Travis CI (that was failing in some configurations). Plus some other minor changes to the config.

--- postsubmit.sh
+++ postsubmit.sh
@@ -6,12 +6,14 @@ mkdir build
 cd build
 
 EXTRA_CMAKE_ARGS=()
-if [[ ${BUILD_TYPE} != "Debug" ]]
+if [[ ${BUILD_TYPE} == "Debug" ]]
 then
+  EXTRA_CMAKE_ARGS+=(-DCMAKE_CXX_FLAGS="-O0 -W -Wall -Werror")
+else
   EXTRA_CMAKE_ARGS+=(-DCMAKE_CXX_FLAGS="-O2 -DNDEBUG -W -Wall -Werror")
 fi
 
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} "${EXTRA_CMAKE_ARGS[@]}"
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCOMPILERS_TO_TEST=${CXX} "${EXTRA_CMAKE_ARGS[@]}"
 
 make -j2 VERBOSE=1
 
