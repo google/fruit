aaea33b6aa5af1ad35447f3a07a935d09ba2a390
Marco Poletti <poletti.marco@gmail.com>
2015-12-01 19:30:39 +0100

    Travis CI config for testing on OSX (4th attempt).

--- travis_ci_install_osx.sh
+++ travis_ci_install_osx.sh
@@ -2,7 +2,17 @@
 
 set -e
 
-brew install gcc48
-brew install valgrind
+install_brew_package() {
+  if brew list -1 | grep -q "^$1\$"; then
+    # Package is installed, upgrade if needed
+    brew outdated "$1" || brew upgrade "$@"
+  else
+    # Package not installed yet, install.
+    brew install "$@"
+  fi
+}
+
+install_brew_package gcc48
+install_brew_package valgrind
 # Note: the lack of quotes is intentional to allow passing options (e.g. "--with-clang" inside COMPILER_TO_INSTALL.
-brew install ${COMPILER_TO_INSTALL}
+install_brew_package ${COMPILER_TO_INSTALL}
