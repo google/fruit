3ed306ac1cd2f894f039d71b427f2ca209c4680b
Marco Poletti <poletti.marco@gmail.com>
2016-02-07 17:52:29 +0000

    A few changes to the Travis CI config, notably: * Don't run tests with ASan with GCC 4.8, it's not supported. * Print the C++ standard library location at startup. * Allow OSX builds/tests to fail, it's not yet officially supported.

--- postsubmit.sh
+++ postsubmit.sh
@@ -6,6 +6,11 @@ set -e
 # ignored).
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
+if [ "$N_JOBS" = "" ]
+then
+  N_JOBS=2
+fi
+
 case "$1" in
 DebugAsan)       CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Debug   -DCMAKE_CXX_FLAGS="-O0"     -DINSTRUMENT_WITH_SANITIZERS=TRUE) ;;
 DebugValgrind)   CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Debug   -DCMAKE_CXX_FLAGS="-O2"     -DRUN_TESTS_UNDER_VALGRIND=TRUE) ;;
@@ -15,7 +20,7 @@ ReleaseValgrind) CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Werr
 esac
 
 run_make() {
-  make -j2 VERBOSE=1
+  make -j$N_JOBS VERBOSE=1
 }
 
 rm -rf build
@@ -30,4 +35,4 @@ cd ..
 
 cd tests
 run_make
-ctest --output-on-failure -j2
+ctest --output-on-failure -j$N_JOBS
