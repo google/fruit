google/fruit

commit 8bb168b783a6cab5ead16ab16c0f451afbdccd2b

Update the Travis CI config so that we test against the clang + libc++ combination too (and in any case, we always specify explicitly what STL type to use).

travis_ci_install_linux.sh
@@ -5,3 +5,7 @@ set -e
 # Always install latest GCC 4.8 to avoid bugs in old STL when compiling with Clang.
 sudo apt-get install -qq --force-yes g++-4.8
 sudo apt-get install -qq --force-yes valgrind ${CXX}
+
+if [ "$STL" == "libc++" ]
+ sudo apt-get install -qq --force-yes libc++-dev
+fi

.travis.yml
@@ -34,40 +34,56 @@ matrix:
     - os: linux
       compiler: gcc
       # ASAN (aka '-fsanitize=undefined,address') is not supported in GCC 4.8.
-      env: MY_CC=gcc-4.8   MY_CXX=g++-4.8 NO_ASAN=1
+      env: MY_CC=gcc-4.8   MY_CXX=g++-4.8     STL=libstdc++ NO_ASAN=1
     - os: linux
       compiler: gcc
-      env: MY_CC=gcc-4.9   MY_CXX=g++-4.9
+      env: MY_CC=gcc-4.9   MY_CXX=g++-4.9     STL=libstdc++
     - os: linux
       compiler: gcc
-      env: MY_CC=gcc-5     MY_CXX=g++-5
+      env: MY_CC=gcc-5     MY_CXX=g++-5       STL=libstdc++
     - os: linux
       compiler: clang
-      env: MY_CC=clang-3.5 MY_CXX=clang++-3.5
+      env: MY_CC=clang-3.5 MY_CXX=clang++-3.5 STL=libstdc++
     - os: linux
       compiler: clang
-      env: MY_CC=clang-3.6 MY_CXX=clang++-3.6
+      env: MY_CC=clang-3.6 MY_CXX=clang++-3.6 STL=libstdc++
     - os: linux
       compiler: clang
-      env: MY_CC=clang-3.7 MY_CXX=clang++-3.7
+      env: MY_CC=clang-3.7 MY_CXX=clang++-3.7 STL=libstdc++
+    - os: linux
+      compiler: clang
+      env: MY_CC=clang-3.5 MY_CXX=clang++-3.5 STL=libc++
+    - os: linux
+      compiler: clang
+      env: MY_CC=clang-3.6 MY_CXX=clang++-3.6 STL=libc++
+    - os: linux
+      compiler: clang
+      env: MY_CC=clang-3.7 MY_CXX=clang++-3.7 STL=libc++
       
       # TODO: Run OS X tests under Valgrind too, once it works.
       # We already run a couple under Valgrind, to see the result.
     - os: osx
       compiler: gcc
-      env: MY_CC=gcc-4.8   MY_CXX=g++-4.8     NO_VALGRIND=1
+      # ASAN (aka '-fsanitize=undefined,address') is not supported in GCC 4.8.
+      env: MY_CC=gcc-4.8   MY_CXX=g++-4.8     STL=libstdc++ NO_VALGRIND=1  NO_ASAN=1
     - os: osx
       compiler: gcc
-      env: MY_CC=gcc-4.9   MY_CXX=g++-4.9     NO_VALGRIND=1
+      env: MY_CC=gcc-4.9   MY_CXX=g++-4.9     STL=libstdc++ NO_VALGRIND=1
     - os: osx
       compiler: gcc
-      env: MY_CC=gcc-5     MY_CXX=g++-5
+      env: MY_CC=gcc-5     MY_CXX=g++-5       STL=libstdc++
     - os: osx
       compiler: clang
-      env: MY_CC=clang-3.5 MY_CXX=clang++-3.5 NO_VALGRIND=1
+      env: MY_CC=clang-3.5 MY_CXX=clang++-3.5 STL=libstdc++ NO_VALGRIND=1
     - os: osx
       compiler: clang
-      env: MY_CC=clang-3.6 MY_CXX=clang++-3.6
+      env: MY_CC=clang-3.6 MY_CXX=clang++-3.6 STL=libstdc++
+    - os: osx
+      compiler: clang
+      env: MY_CC=clang-3.5 MY_CXX=clang++-3.5 STL=libc++ NO_VALGRIND=1
+    - os: osx
+      compiler: clang
+      env: MY_CC=clang-3.6 MY_CXX=clang++-3.6 STL=libc++
   allow_failures:
     - os: osx
 

postsubmit.sh
@@ -11,12 +11,18 @@ then
   N_JOBS=2
 fi
 
+if [ "$STL" = "" ]
+then
+  echo "Error: you need to specify the STL environment variable."
+  exit 1
+fi
+
 case "$1" in
-DebugAsan)       CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Debug   -DCMAKE_CXX_FLAGS="-O0"     -DINSTRUMENT_WITH_SANITIZERS=TRUE) ;;
-DebugValgrind)   CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Debug   -DCMAKE_CXX_FLAGS="-O2"     -DRUN_TESTS_UNDER_VALGRIND=TRUE) ;;
-ReleasePlain)    CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Werror") ;;
-ReleaseValgrind) CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Werror" -DRUN_TESTS_UNDER_VALGRIND=TRUE) ;;
-*) echo "Error: you need to specify one of the supported postsubmit modes (see postsubmit.sh)." ;;
+DebugAsan)       CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Debug   -DCMAKE_CXX_FLAGS="-stdlib=$STL -O0"     -DINSTRUMENT_WITH_SANITIZERS=TRUE) ;;
+DebugValgrind)   CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Debug   -DCMAKE_CXX_FLAGS="-stdlib=$STL -O2"     -DRUN_TESTS_UNDER_VALGRIND=TRUE) ;;
+ReleasePlain)    CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-stdlib=$STL -Werror") ;;
+ReleaseValgrind) CMAKE_ARGS=(-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-stdlib=$STL -Werror" -DRUN_TESTS_UNDER_VALGRIND=TRUE) ;;
+*) echo "Error: you need to specify one of the supported postsubmit modes (see postsubmit.sh)."; exit 1 ;;
 esac
 
 run_make() {

travis_ci_install_osx.sh
@@ -24,7 +24,7 @@ case "${CXX}" in
 g++-4.8)     install_brew_package homebrew/versions/gcc48 ;;
 g++-4.9)     install_brew_package homebrew/versions/gcc49 ;;
 g++-5)       install_brew_package homebrew/versions/gcc5 ;;
-clang++-3.5) install_brew_package homebrew/versions/llvm35 --with-clang ;;
-clang++-3.6) install_brew_package homebrew/versions/llvm36 --with-clang ;;
+clang++-3.5) install_brew_package homebrew/versions/llvm35 --with-clang --with-libcxx;;
+clang++-3.6) install_brew_package homebrew/versions/llvm36 --with-clang --with-libcxx;;
 *) echo "Compiler not supported: ${CXX}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac

commit 4fe642224375fde2bd16ef4685016d336a83dfee

Fix syntax error in Travis config, introduced in commit 8bb168b.

travis_ci_install_linux.sh
@@ -7,5 +7,6 @@ sudo apt-get install -qq --force-yes g++-4.8
 sudo apt-get install -qq --force-yes valgrind ${CXX}
 
 if [ "$STL" == "libc++" ]
+then
  sudo apt-get install -qq --force-yes libc++-dev
 fi