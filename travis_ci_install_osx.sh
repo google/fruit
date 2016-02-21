9c18e0a2c26f41cd088df051ea14102e33a96f8c
Marco Poletti <poletti.marco@gmail.com>
2016-02-21 10:05:34 +0000

    Some changes to the Travis CI config: * fix a bug preventing the compiler/STL version from being printed in some cases * Remove allowed_failures and disable combinations that are known not to work.

--- travis_ci_install_osx.sh
+++ travis_ci_install_osx.sh
@@ -28,5 +28,6 @@ g++-5)       install_brew_package homebrew/versions/gcc5 ;;
 clang++)     ;;
 clang++-3.5) install_brew_package homebrew/versions/llvm35 --with-clang --with-libcxx;;
 clang++-3.6) install_brew_package homebrew/versions/llvm36 --with-clang --with-libcxx;;
+clang++-3.7) install_brew_package homebrew/versions/llvm37 --with-clang --with-libcxx;;
 *) echo "Compiler not supported: ${CXX}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
