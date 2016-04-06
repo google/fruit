9c38d6a162354a53ac06369ad2d989f76c475f07
Marco Poletti <poletti.marco@gmail.com>
2016-04-06 12:03:48 +0200

    Use Docker in Travis CI. Also start testing under Ubuntu 15.10 (as well as 14.04).

--- scripts/travis_ci_before_install_linux.sh
+++ scripts/travis_ci_before_install_linux.sh
@@ -2,10 +2,34 @@
 
 set -e
 
+docker pull ubuntu:$UBUNTU
+docker run -d -i --name fruit --privileged ubuntu:$UBUNTU
+docker exec fruit mkdir fruit
+docker cp . fruit:fruit
+docker attach fruit <<EOF
+cd fruit
+EOF
+
+case "$UBUNTU" in
+14.04)
+    REPO_NAME="trusty";;
+15.10)
+    REPO_NAME="xenial";;
+*)
+    echo "Unrecognized Ubuntu version: $UBUNTU"
+    exit 1
+esac
+
+docker attach fruit <<EOF
+set -e
+
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main'
-sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main'
-sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main'
-sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main'
+
+sudo add-apt-repository -y "deb http://llvm.org/apt/trusty/ llvm-toolchain-${REPO_NAME} main"
+sudo add-apt-repository -y "deb http://llvm.org/apt/trusty/ llvm-toolchain-${REPO_NAME}-3.6 main"
+sudo add-apt-repository -y "deb http://llvm.org/apt/trusty/ llvm-toolchain-${REPO_NAME}-3.7 main"
+sudo add-apt-repository -y "deb http://llvm.org/apt/trusty/ llvm-toolchain-${REPO_NAME}-3.8 main"
+
 sudo apt-get update -qq
+EOF
--- scripts/travis_ci_install_linux.sh
+++ scripts/travis_ci_install_linux.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+docker attach fruit <<EOF
+set -e
+
 # Always install latest GCC 4.8 to avoid bugs in old STL when compiling with Clang.
 sudo apt-get install -qq --force-yes g++-4.8
 sudo apt-get install -qq --force-yes valgrind ${CXX}
@@ -10,3 +13,4 @@ if [ "$STL" == "libc++" ]
 then
  sudo apt-get install -qq --force-yes libc++-dev
 fi
+EOF
