af3ff96d5d8d02d8f6e587ee431bc753bcce86fa
Marco Poletti <poletti.marco@gmail.com>
2016-11-12 18:09:32 +0000

    Revert to using Clang 3.8 in OS X tests, the Clang 3.9 tests are failing due to an installation issue.

--- extras/scripts/travis_yml_generator.py
+++ extras/scripts/travis_yml_generator.py
@@ -155,7 +155,7 @@ add_osx_tests(compiler='clang-3.6', stl='libc++', asan=False, ubsan=False)
 # ASan/UBSan are disabled because it would hit errors like:
 # ld: file not found: [...]/libclang_rt.asan_osx_dynamic.dylib
 # Not sure if that's a limitation of Clang 3.8 on OS X or just of the brew-provided binaries.
-add_osx_tests(compiler='clang-3.9', stl='libc++', asan=False, ubsan=False, smoke_tests=['DebugPlain'])
+add_osx_tests(compiler='clang-3.8', stl='libc++', asan=False, ubsan=False, smoke_tests=['DebugPlain'])
 
 # UBSan is disabled because AppleClang does not support -fsanitize=undefined.
 add_osx_tests(compiler='clang-default', xcode_version='7.1', stl='libc++', ubsan=False)
