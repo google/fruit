google/fruit

commit fa6a0494109a850ca635de6d33e6ddbc55015c3a

Don't call alignof(T) for abstract types. This is not supported in Visual Studio (and Fruit never needs this value for abstract types anyway).

include/fruit/impl/util/type_info.defn.h
@@ -24,15 +24,46 @@
 
 namespace fruit {
 namespace impl {
-  
+
+template <typename T, bool is_abstract = std::is_abstract<T>::value>
+struct GetConcreteTypeInfo {
+  constexpr TypeInfo::ConcreteTypeInfo operator()() {
+    return TypeInfo::ConcreteTypeInfo{
+        sizeof(T),
+        alignof(T),
+        std::is_trivially_destructible<T>::value,
+#ifdef FRUIT_EXTRA_DEBUG
+        false /* is_abstract */,
+#endif
+    };
+  }
+};
+
+// For abstract types we don't need the real information.
+// Also, some compilers might report compile errors in this case, for example alignof(T) doesn't work in Visual Studio
+// when T is an abstract type.
+template <typename T>
+struct GetConcreteTypeInfo<T, true> {
+  constexpr TypeInfo::ConcreteTypeInfo operator()() {
+    return TypeInfo::ConcreteTypeInfo{
+        0 /* type_size */,
+        0 /* type_alignment */,
+        false /* is_trivially_destructible */,
+#ifdef FRUIT_EXTRA_DEBUG
+        true /* is_abstract */,
+#endif
+    };
+  }
+};
+
+
 // This should only be used if RTTI is disabled. Use the other constructor if possible.
-inline constexpr TypeInfo::TypeInfo(std::size_t type_size, std::size_t type_alignment, bool is_trivially_destructible)
-: info(nullptr), type_size(type_size), type_alignment(type_alignment), is_trivially_destructible(is_trivially_destructible) {
+inline constexpr TypeInfo::TypeInfo(ConcreteTypeInfo concrete_type_info)
+  : info(nullptr), concrete_type_info(concrete_type_info) {
 }
 
-inline constexpr TypeInfo::TypeInfo(const std::type_info& info, std::size_t type_size, std::size_t type_alignment, 
-                    bool is_trivially_destructible)
-: info(&info), type_size(type_size), type_alignment(type_alignment), is_trivially_destructible(is_trivially_destructible) {
+inline constexpr TypeInfo::TypeInfo(const std::type_info& info, ConcreteTypeInfo concrete_type_info)
+  : info(&info), concrete_type_info(concrete_type_info) {
 }
 
 inline std::string TypeInfo::name() const {
@@ -43,15 +74,24 @@ inline std::string TypeInfo::name() const {
 }
 
 inline size_t TypeInfo::size() const {
-  return type_size;
+#ifdef FRUIT_EXTRA_DEBUG
+  FruitAssert(!concrete_type_info.is_abstract);
+#endif
+  return concrete_type_info.type_size;
 }  
 
 inline size_t TypeInfo::alignment() const {
-  return type_alignment;
+#ifdef FRUIT_EXTRA_DEBUG
+  FruitAssert(!concrete_type_info.is_abstract);
+#endif
+  return concrete_type_info.type_alignment;
 }  
 
 inline bool TypeInfo::isTriviallyDestructible() const {
-  return is_trivially_destructible;
+#ifdef FRUIT_EXTRA_DEBUG
+  FruitAssert(!concrete_type_info.is_abstract);
+#endif
+  return concrete_type_info.is_trivially_destructible;
 }
   
 inline TypeId::operator std::string() const {
@@ -74,9 +114,9 @@ template <typename T>
 struct GetTypeInfoForType {
   constexpr TypeInfo operator()() const {
 #ifdef FRUIT_HAS_TYPEID
-    return TypeInfo(typeid(T), sizeof(T), alignof(T), std::is_trivially_destructible<T>::value);
+    return TypeInfo(typeid(T), GetConcreteTypeInfo<T>()());
 #else
-    return TypeInfo(sizeof(T), alignof(T), std::is_trivially_destructible<T>::value);
+    return TypeInfo(GetConcreteTypeInfo<T>()());
 #endif
   };
 };
@@ -85,9 +125,9 @@ template <typename Annotation, typename T>
 struct GetTypeInfoForType<fruit::Annotated<Annotation, T>> {
   constexpr TypeInfo operator()() const {
 #ifdef FRUIT_HAS_TYPEID
-    return TypeInfo(typeid(fruit::Annotated<Annotation, T>), sizeof(T), alignof(T), std::is_trivially_destructible<T>::value);
+    return TypeInfo(typeid(fruit::Annotated<Annotation, T>), GetConcreteTypeInfo<T>()());
 #else
-    return TypeInfo(sizeof(T), alignof(T), std::is_trivially_destructible<T>::value);
+    return TypeInfo(GetConcreteTypeInfo<T>()());
 #endif
   };
 };

include/fruit/impl/util/type_info.h
@@ -29,11 +29,22 @@ namespace impl {
 // Similar to std::type_index, but with a constexpr constructor and also storing the type size and alignment.
 // Also guaranteed to be aligned, to allow storing a TypeInfo and 1 bit together in the size of a void*.
 struct alignas(1) alignas(void*) TypeInfo {
-  // This should only be used if RTTI is disabled. Use the other constructor if possible.
-  constexpr TypeInfo(std::size_t type_size, std::size_t type_alignment, bool is_trivially_destructible);
 
-  constexpr TypeInfo(const std::type_info& info, std::size_t type_size, std::size_t type_alignment, 
-                     bool is_trivially_destructible);
+  struct ConcreteTypeInfo {
+    // These fields are allowed to have dummy values for abstract types.
+    std::size_t type_size;
+    std::size_t type_alignment;
+    bool is_trivially_destructible;
+
+#ifdef FRUIT_EXTRA_DEBUG
+    bool is_abstract;
+#endif
+  };
+
+  // This should only be used if RTTI is disabled. Use the other constructor if possible.
+  constexpr TypeInfo(ConcreteTypeInfo concrete_type_info);
+
+  constexpr TypeInfo(const std::type_info& info, ConcreteTypeInfo concrete_type_info);
 
   std::string name() const;
 
@@ -47,10 +58,7 @@ private:
   // The std::type_info struct associated with the type, or nullptr if RTTI is disabled.
   // This is only used for the type name.
   const std::type_info* info;
-  
-  std::size_t type_size;
-  std::size_t type_alignment;
-  bool is_trivially_destructible;
+  ConcreteTypeInfo concrete_type_info;
 };
 
 struct TypeId {

commit 8a8c76f8d4ffaacb7fa9304812a2ef7828e0f325

Add an include to fix a compile error introduced by fa6a049 (that happened only in some compilation modes).

include/fruit/impl/util/type_info.defn.h
@@ -21,6 +21,7 @@
 
 #include <fruit/impl/fruit-config.h>
 #include <fruit/fruit_forward_decls.h>
+#include <fruit/impl/fruit_assert.h>
 
 namespace fruit {
 namespace impl {