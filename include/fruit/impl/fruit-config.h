google/fruit

commit 905dc09804c6f14bdaa063d37d71346a62bc37ea

Support building Fruit with MSVC.
Current limitations:
* Tests must be run from the command-line, using the "Native Tools Command Prompt" that comes with MSVC. Running tests from within MSVC doesn't work.
* Tests must be run serially (-j 1), otherwise there might be test failures with errors like "fatal error C1033: cannot open program database 'C:\Some\Path\vc140.pdb'"
* Building Fruit as a shared library (.dll) is not yet supported, Fruit can only be built as a static library (.lib).

include/fruit/impl/fruit-config.h
@@ -30,16 +30,32 @@
 #endif
 
 #if FRUIT_HAS_STD_IS_TRIVIALLY_COPYABLE
-#define FRUIT_IS_TRIVIALLY_COPYABLE(T) std::is_trivially_copyable<T>::value
+#if FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (std::is_trivially_copyable<T>::value || (std::is_empty<T>::value && std::is_trivially_copy_constructible<T>::value))
+#else // !FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (std::is_trivially_copyable<T>::value)
+#endif // FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #elif FRUIT_HAS_IS_TRIVIALLY_COPYABLE
-#define FRUIT_IS_TRIVIALLY_COPYABLE(T) __is_trivially_copyable(T)
+#if FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__is_trivially_copyable(T) || (std::is_empty<T>::value && std::is_trivially_copy_constructible<T>::value))
+#else // !FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__is_trivially_copyable(T))
+#endif FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #elif FRUIT_HAS_HAS_TRIVIAL_COPY
 // The compiler doesn't support __is_trivially_copyable (nor is std::is_trivially_copyable
 // supported by the library). We use this check as a proxy, but it's not exactly the same thing.
-#define FRUIT_IS_TRIVIALLY_COPYABLE(T) __has_trivial_copy(T)
+#if FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__has_trivial_copy(T) || (std::is_empty<T>::value && std::is_trivially_copy_constructible<T>::value))
+#else // !FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__has_trivial_copy(T))
+#endif FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #else
 // We use the standard one, but most likely it won't work.
-#define FRUIT_IS_TRIVIALLY_COPYABLE(T) std::is_trivially_copyable<T>::value
+#if FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (std::is_trivially_copyable<T>::value || (std::is_empty<T>::value && std::is_trivially_copy_constructible<T>::value))
+#else // !FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#define FRUIT_IS_TRIVIALLY_COPYABLE(T) (std::is_trivially_copyable<T>::value)
+#endif // FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #endif
 
 #endif // FRUIT_CONFIG_H

.gitignore
@@ -7,3 +7,10 @@ bench_results
 *.pyc
 /build*
 /cmake-build-debug
+/cmake-build-release
+/.vs
+/Debug
+/*.sln
+/*.vcxproj
+/*.vcxproj.filters
+/CMakeSettings.json

CMakeLists.txt
@@ -1,21 +1,34 @@
 project(Fruit)
 cmake_minimum_required(VERSION 2.8)
+cmake_policy(SET CMP0054 NEW)
 
 # CMake on OSX likes to see this set explicitly, otherwise it outputs a warning.
 set(CMAKE_MACOSX_RPATH 1)
 
-# Build shared libraries by default
-option(BUILD_SHARED_LIBS "Build shared library" ON)
-
-if(NOT "${CMAKE_BUILD_TYPE}")
-  set(CMAKE_BUILD_TYPE "Release")
+if(NOT "${CMAKE_BUILD_TYPE}" MATCHES "^(Debug|Release)$")
+  message(FATAL_ERROR "Please re-run CMake, specifying -DCMAKE_BUILD_TYPE=Debug or -DCMAKE_BUILD_TYPE=Release.")
 endif()
+
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+    # Build a static library by default, since building Fruit as a shared library is not supported with MSVC.
+    option(BUILD_SHARED_LIBS "Build shared library" OFF)
+
+    if ("${BUILD_SHARED_LIBS}")
+        message(FATAL_ERROR "Building Fruit as a shared library (dll) is not supported when building with MSVC.")
+    endif()
+else()
+    # Otherwise, build a shared library by default.
+    option(BUILD_SHARED_LIBS "Build shared library" ON)
+endif()
+
+# The Visual Studio CMake generators default to multiple configurations, but Fruit doesn't support multi-configuration build directories.
+set(CMAKE_CONFIGURATION_TYPES "${CMAKE_BUILD_TYPE}")
 
 if(NOT "${RUNTIME_OUTPUT_DIRECTORY}")
-  set(RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+  set(RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
 endif()
 
-if(NOT "${CMAKE_CXX_COMPILER_ID}" MATCHES "^(GNU|Clang|AppleClang)$")
+if(NOT "${CMAKE_CXX_COMPILER_ID}" MATCHES "^(GNU|Clang|AppleClang|MSVC)$")
   message(WARNING "Compiler not officially supported: ${CMAKE_CXX_COMPILER_ID}")
   # Full list of possible values at https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER_ID.html .
   # Major compilers not currently supported:
@@ -30,7 +43,8 @@ endif()
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "^(GNU|Clang|Intel|AppleClang)$")
   set(FRUIT_ADDITIONAL_COMPILE_FLAGS "${FRUIT_ADDITIONAL_COMPILE_FLAGS} -std=c++11 -W -Wall -Wno-constexpr-not-const -Wno-unknown-warning-option -Wno-missing-braces")
 elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "^(MSVC)$")
-  set(FRUIT_ADDITIONAL_COMPILE_FLAGS "${FRUIT_ADDITIONAL_COMPILE_FLAGS} /Wall /D_SCL_SECURE_NO_WARNINGS")
+  # TODO: we currently disable the warning C4709 because MSVC emits it even when there is no reason to. Re-enable it when possible.
+  set(FRUIT_ADDITIONAL_COMPILE_FLAGS "${FRUIT_ADDITIONAL_COMPILE_FLAGS} /nologo /FS /W4 /wd4324 /wd4709 /wd4459 /D_SCL_SECURE_NO_WARNINGS")
 endif()
 
 if("${WIN32}")
@@ -72,7 +86,12 @@ add_definitions(${FRUIT_ADDITIONAL_COMPILE_FLAGS})
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${FRUIT_ADDITIONAL_LINKER_FLAGS}")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${FRUIT_ADDITIONAL_LINKER_FLAGS}")
 set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${FRUIT_ADDITIONAL_LINKER_FLAGS}")
-set(FRUIT_COMPILE_FLAGS "${CMAKE_CXX_FLAGS} ${FRUIT_ADDITIONAL_COMPILE_FLAGS}")
+
+if ("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+    set(FRUIT_COMPILE_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE} ${FRUIT_ADDITIONAL_COMPILE_FLAGS}")
+else()
+    set(FRUIT_COMPILE_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG} ${FRUIT_ADDITIONAL_COMPILE_FLAGS}")
+endif()
 
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
 include_directories(${CMAKE_BINARY_DIR}/include)
@@ -92,13 +111,19 @@ set(CTEST_TEST_TIMEOUT 20 CACHE STRING "Maximum test time, before CTest will kil
 add_subdirectory(configuration)
 add_subdirectory(src)
 
+option(BUILD_TESTS_IN_RELEASE_MODE "Build tests as part of the ALL target even in Release mode" OFF)
+
 if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
   # Do not exclude these from "make all" in debug mode, they must build.
   add_subdirectory(examples)
   add_subdirectory(tests)
 else()
   add_subdirectory(examples EXCLUDE_FROM_ALL)
-  add_subdirectory(tests EXCLUDE_FROM_ALL)
+  if ("${BUILD_TESTS_IN_RELEASE_MODE}")
+    add_subdirectory(tests)
+  else()
+    add_subdirectory(tests EXCLUDE_FROM_ALL)
+  endif()
 endif()
 
 add_subdirectory(extras EXCLUDE_FROM_ALL)

configuration/CMakeLists.txt
@@ -1,8 +1,16 @@
 
 include(CheckCXXSourceCompiles)
 
-set(CMAKE_REQUIRED_DEFINITIONS "${FRUIT_COMPILE_FLAGS}")
-set(CMAKE_REQUIRED_FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
+set(CMAKE_REQUIRED_FLAGS "${FRUIT_COMPILE_FLAGS} ${CMAKE_EXE_LINKER_FLAGS}")
+
+CHECK_CXX_SOURCE_COMPILES("
+int main() {}
+"
+FRUIT_TRIVIAL_SOURCE_COMPILES)
+
+if (NOT "${FRUIT_TRIVIAL_SOURCE_COMPILES}")
+    message(FATAL_ERROR "A trivial program with an empty main doesn't compile, something is wrong with your compiler and/or with your compiler flags.")
+endif()
 
 CHECK_CXX_SOURCE_COMPILES("
 template <typename T, typename U>
@@ -57,6 +65,16 @@ int main() {
 FRUIT_HAS_STD_IS_TRIVIALLY_COPYABLE)
 
 CHECK_CXX_SOURCE_COMPILES("
+#include <type_traits>
+int main() {
+  bool b = std::is_trivially_copy_constructible<int>::value;
+  (void) b;
+  return 0;
+}
+"
+FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE)
+
+CHECK_CXX_SOURCE_COMPILES("
 #include <cstddef>
 using X = std::max_align_t;
 int main() {
@@ -73,6 +91,16 @@ int main() {
 }
 "
 FRUIT_HAS_TYPEID)
+
+CHECK_CXX_SOURCE_COMPILES("
+#include <typeinfo>
+int main() {
+  constexpr static const std::type_info& x = typeid(int);
+  (void) x;
+  return 0;
+}
+"
+FRUIT_HAS_CONSTEXPR_TYPEID)
 
 CHECK_CXX_SOURCE_COMPILES("
 #include <cxxabi.h>

configuration/bazel/fruit/impl/fruit-config-base.h
@@ -39,8 +39,14 @@
 // Ignored if FRUIT_HAS_STD_IS_TRIVIALLY_COPYABLE is set.
 #define FRUIT_HAS_IS_TRIVIALLY_COPYABLE 1
 
+// Whether the compiler defines std::is_trivially_copy_constructible.
+#define FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE 1
+
 // Whether typeid() is available. Typically, it is unless RTTI is disabled.
 #define FRUIT_HAS_TYPEID 1
+
+// Whether typeid() is constexpr. Typically, it is except in MSVC.
+#define FRUIT_HAS_CONSTEXPR_TYPEID 1
 
 // Whether abi::__cxa_demangle() is available after including cxxabi.h.
 #define FRUIT_HAS_CXA_DEMANGLE 1

configuration/fruit-config-base.h.in
@@ -22,8 +22,10 @@
 #cmakedefine FRUIT_HAS_IS_TRIVIALLY_COPYABLE 1
 #cmakedefine FRUIT_HAS_MAX_ALIGN_T 1
 #cmakedefine FRUIT_HAS_STD_IS_TRIVIALLY_COPYABLE 1
+#cmakedefine FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE 1
 #cmakedefine FRUIT_HAS_STD_MAX_ALIGN_T 1
 #cmakedefine FRUIT_HAS_TYPEID 1
+#cmakedefine FRUIT_HAS_CONSTEXPR_TYPEID 1
 #cmakedefine FRUIT_HAS_CXA_DEMANGLE 1
 
 #endif // FRUIT_CONFIG_BASE_H

extras/bazel_root/third_party/fruit/build
@@ -1,1 +1,1 @@
-../../../../build
+../../../../BUILD

include/fruit/impl/component.defn.h
@@ -34,16 +34,16 @@ template <typename... PreviousBindings>
 struct OpForComponent {
   template <typename Comp>
   using ConvertTo = Eval<
-      Call(ReverseComposeFunctors(ComponentFunctor(ConvertComponent, Comp),
-                                  ProcessDeferredBindings,
-                                  Id<ProcessBinding(PreviousBindings)>...),
-          ConstructComponentImpl())>;
+      Call(ReverseComposeFunctors(Id<ComponentFunctor(ConvertComponent, Comp)>,
+		                          ProcessDeferredBindings,
+		                          Id<ProcessBinding(PreviousBindings)>...),
+           ConstructComponentImpl())>;
   
   template <typename Binding>
   using AddBinding = Eval<
-      Call(ReverseComposeFunctors(ProcessBinding(Binding),
-                                  Id<ProcessBinding(PreviousBindings)>...),
-           ConstructComponentImpl())>;
+	  Call(ReverseComposeFunctors(Id<ProcessBinding(Binding)>,
+		                          Id<ProcessBinding(PreviousBindings)>...),
+		   ConstructComponentImpl())>;
 };
 } // namespace meta
 } // namespace impl

commit 64127456ebcd67c6f25033c781f9b1a2a6839076

Fix another build error introduced by commit 905dc09.

include/fruit/impl/fruit-config.h
@@ -40,7 +40,7 @@
 #define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__is_trivially_copyable(T) || (std::is_empty<T>::value && std::is_trivially_copy_constructible<T>::value))
 #else // !FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__is_trivially_copyable(T))
-#endif FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#endif // FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #elif FRUIT_HAS_HAS_TRIVIAL_COPY
 // The compiler doesn't support __is_trivially_copyable (nor is std::is_trivially_copyable
 // supported by the library). We use this check as a proxy, but it's not exactly the same thing.
@@ -48,7 +48,7 @@
 #define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__has_trivial_copy(T) || (std::is_empty<T>::value && std::is_trivially_copy_constructible<T>::value))
 #else // !FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #define FRUIT_IS_TRIVIALLY_COPYABLE(T) (__has_trivial_copy(T))
-#endif FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
+#endif // FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE
 #else
 // We use the standard one, but most likely it won't work.
 #if FRUIT_HAS_STD_IS_TRIVIALLY_COPY_CONSTRUCTIBLE