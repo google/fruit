42a37e169f5e3354dc72d34547e5d5dc77ec8bd9
Marco Poletti <poletti.marco@gmail.com>
2017-03-18 17:48:13 +0000

    Try running postsubmit.bat with CALL instead of directly, to see if that works.
    Also add some echo statements in postsubmit.bat to help distinguish the various phases.

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -1,17 +1,27 @@
 
+echo Postsubmit.bat started.
+
 if exist %VCVARSALL_SCRIPT% (
   CALL %VCVARSALL_SCRIPT% amd64
 )
 
+echo Creating build directory.
+
 mkdir C:\Fruit\build-%CONFIGURATION%
 cd C:\Fruit\build-%CONFIGURATION%
 
+echo Running CMake.
+
 %CMAKE_PATH%\cmake.exe -G %CMAKE_GENERATOR% .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR=%BOOST_DIR% -DBUILD_TESTS_IN_RELEASE_MODE=True
+
+echo Building Fruit.
 
 IF %CMAKE_GENERATOR%=="MinGW Makefiles" (
   mingw32-make -j12
 ) ELSE (
   msbuild ALL_BUILD.vcxproj
 )
+
+echo Running tests.
 
 %CMAKE_PATH%\ctest.exe -j 1 --output-on-failure -C %CONFIGURATION%
