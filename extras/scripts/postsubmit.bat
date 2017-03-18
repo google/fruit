0b31f7e216e37e9eb8de1fb6e3dc37dac5ffa1d2
Marco Poletti <poletti.marco@gmail.com>
2017-03-18 18:02:40 +0000

    Various improvements to the AppVeyor config/script:
    * More debug echoes
    * Exit on the first error
    * Define the VCVARSALL_SCRIPT env variable when testing with MinGW

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -1,8 +1,11 @@
 
+echo on
+
 echo Postsubmit.bat started.
 
 if exist %VCVARSALL_SCRIPT% (
-  CALL %VCVARSALL_SCRIPT% amd64
+  echo Running VCVARSALL script: %VCVARSALL_SCRIPT%
+  CALL %VCVARSALL_SCRIPT% amd64 || exit /b %errorlevel%
 )
 
 echo Creating build directory.
@@ -12,16 +15,16 @@ cd C:\Fruit\build-%CONFIGURATION%
 
 echo Running CMake.
 
-%CMAKE_PATH%\cmake.exe -G %CMAKE_GENERATOR% .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR=%BOOST_DIR% -DBUILD_TESTS_IN_RELEASE_MODE=True
+%CMAKE_PATH%\cmake.exe -G %CMAKE_GENERATOR% .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR=%BOOST_DIR% -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b %errorlevel%
 
 echo Building Fruit.
 
 IF %CMAKE_GENERATOR%=="MinGW Makefiles" (
-  mingw32-make -j12
+  mingw32-make -j12 || exit /b %errorlevel%
 ) ELSE (
-  msbuild ALL_BUILD.vcxproj
+  msbuild ALL_BUILD.vcxproj || exit /b %errorlevel%
 )
 
 echo Running tests.
 
-%CMAKE_PATH%\ctest.exe -j 1 --output-on-failure -C %CONFIGURATION%
+%CMAKE_PATH%\ctest.exe -j 1 --output-on-failure -C %CONFIGURATION% || exit /b %errorlevel%
