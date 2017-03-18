8399370720acda1cc76c446c063af813cc4d00ad
Marco Poletti <poletti.marco@gmail.com>
2017-03-18 19:16:49 +0000

    Add MinGW's bin directory to PATH when testing Fruit with MinGW on AppVeyor. MinGW's GCC fails to start if MinGW is not in PATH.

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -7,6 +7,10 @@ if not "%VCVARSALL_SCRIPT%" == "" (
   CALL "%VCVARSALL_SCRIPT%" amd64 || exit /b 1
 )
 
+if not "%MINGW_PATH%" == "" (
+  SET PATH=%MINGW_PATH%;%PATH%
+)
+
 mkdir C:\Fruit\build-%CONFIGURATION%
 cd C:\Fruit\build-%CONFIGURATION%
 
@@ -14,7 +18,7 @@ cd C:\Fruit\build-%CONFIGURATION%
 cmake.exe -G "%CMAKE_GENERATOR%" .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR="%BOOST_DIR%" -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b 1
 
 IF "%CMAKE_GENERATOR%"=="MinGW Makefiles" (
-  "%MINGW_PATH%\mingw32-make" -j12 || exit /b 1
+  mingw32-make" -j12 || exit /b 1
 ) ELSE (
   msbuild ALL_BUILD.vcxproj || exit /b 1
 )
