0f06c03edfe8429654e63d4751cf06f0c8e3db22
Marco Poletti <poletti.marco@gmail.com>
2017-03-18 19:11:00 +0000

    Quote variables in the AppVeyor script directly instead of the AppVeyor config, to avoid having quotes where there shouldn't be any (e.g. PATH).

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -3,18 +3,18 @@ echo on
 
 set PATH=C:\Windows\system32;C:\Windows;%PYTHON3_PATH%;%CMAKE_PATH%;
 
-if not %VCVARSALL_SCRIPT% == "" (
-  CALL %VCVARSALL_SCRIPT% amd64 || exit /b 1
+if not "%VCVARSALL_SCRIPT%" == "" (
+  CALL "%VCVARSALL_SCRIPT%" amd64 || exit /b 1
 )
 
 mkdir C:\Fruit\build-%CONFIGURATION%
 cd C:\Fruit\build-%CONFIGURATION%
 
 
-cmake.exe -G %CMAKE_GENERATOR% .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR=%BOOST_DIR% -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b 1
+cmake.exe -G "%CMAKE_GENERATOR%" .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR="%BOOST_DIR%" -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b 1
 
-IF %CMAKE_GENERATOR%=="MinGW Makefiles" (
-  %MINGW_PATH%\mingw32-make -j12 || exit /b 1
+IF "%CMAKE_GENERATOR%"=="MinGW Makefiles" (
+  "%MINGW_PATH%\mingw32-make" -j12 || exit /b 1
 ) ELSE (
   msbuild ALL_BUILD.vcxproj || exit /b 1
 )
