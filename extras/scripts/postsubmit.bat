d6c59cd46f9d963567f3c55a1cbedcede9a6849c
Marco Poletti <poletti.marco@gmail.com>
2017-03-18 20:06:47 +0000

    Don't expand variables that might contain parentheses in (...) blocks of the AppVeyor script. This confuses CMD.exe.

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -3,22 +3,9 @@ echo on
 
 set PATH=C:\Windows\system32;C:\Windows;%PYTHON3_PATH%;%CMAKE_PATH%;
 
-if not "%VCVARSALL_DIR%" == "" (
-  rem TODO: remove these commands
-  dir "C:\Program Files (x86)\"
-  dir "C:\Program Files (x86)\Microsoft Visual Studio\"
-  dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\"
-  cd "C:\Program Files (x86)\"
-  dir /s vcvarsall.bat
+if not "%VCVARSALL_DIR%" == "" CALL "%VCVARSALL_DIR%\vcvarsall.bat" amd64
 
-
-
-  CALL "%VCVARSALL_DIR%\vcvarsall.bat" amd64
-)
-
-if not "%MINGW_PATH%" == "" (
-  SET PATH=%MINGW_PATH%;%PATH%
-)
+if not "%MINGW_PATH%" == "" SET PATH=%MINGW_PATH%;%PATH%
 
 mkdir C:\Fruit\build-%CONFIGURATION%
 cd C:\Fruit\build-%CONFIGURATION%
@@ -27,7 +14,7 @@ cd C:\Fruit\build-%CONFIGURATION%
 cmake.exe -G "%CMAKE_GENERATOR%" .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR="%BOOST_DIR%" -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b 1
 
 IF "%CMAKE_GENERATOR%"=="MinGW Makefiles" (
-  mingw32-make" -j12 || exit /b 1
+  mingw32-make -j12 || exit /b 1
 ) ELSE (
   msbuild ALL_BUILD.vcxproj || exit /b 1
 )
