70a5537f7116276ee1c12053e0b91fe2c1073acc
Marco Poletti <poletti.marco@gmail.com>
2017-03-18 18:57:17 +0000

    Set PATH to a fixed string in the AppVeyor script, to avoid undesired interference from directories in the default PATH.
    Also remove some no-longer-useful debug prints.

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -1,41 +1,22 @@
 
 echo on
 
-echo Postsubmit.bat started.
-
-set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-
-echo PATH changed.
+set PATH=C:\Windows\system32;C:\Windows;%PYTHON3_PATH%;%CMAKE_PATH%;
 
 if not %VCVARSALL_SCRIPT% == "" (
-  echo Running VCVARSALL script: %VCVARSALL_SCRIPT%
   CALL %VCVARSALL_SCRIPT% amd64 || exit /b 1
 )
-
-echo Creating build directory.
 
 mkdir C:\Fruit\build-%CONFIGURATION%
 cd C:\Fruit\build-%CONFIGURATION%
 
-rem TODO: Remove the following 5 commands, they are only for debugging.
-echo Looking for CMake executable:
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake"
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake"
-dir %CMAKE_PATH%
 
-echo Running CMake.
-
-%CMAKE_PATH%\cmake.exe -G %CMAKE_GENERATOR% .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR=%BOOST_DIR% -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b 1
-
-echo Building Fruit.
+cmake.exe -G %CMAKE_GENERATOR% .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBOOST_DIR=%BOOST_DIR% -DBUILD_TESTS_IN_RELEASE_MODE=True || exit /b 1
 
 IF %CMAKE_GENERATOR%=="MinGW Makefiles" (
-  mingw32-make -j12 || exit /b 1
+  %MINGW_PATH%\mingw32-make -j12 || exit /b 1
 ) ELSE (
   msbuild ALL_BUILD.vcxproj || exit /b 1
 )
 
-echo Running tests.
-
-%CMAKE_PATH%\ctest.exe -j 1 --output-on-failure -C %CONFIGURATION% || exit /b 1
+ctest.exe -j 1 --output-on-failure -C %CONFIGURATION% || exit /b 1
