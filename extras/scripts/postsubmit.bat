7168d34e5280df435794306d5c8867a3abce4627
Marco Poletti <poletti.marco@gmail.com>
2017-03-19 11:34:39 +0000

    Add more debug prints to the AppVeyor test script.

--- extras/scripts/postsubmit.bat
+++ extras/scripts/postsubmit.bat
@@ -15,6 +15,7 @@ cd C:\Fruit\build-%CONFIGURATION%
 rem These are just for debugging.
 where cmake.exe
 cmake.exe --help
+dir "%MINGW_PATH%"
 
 cmake.exe -G "%CMAKE_GENERATOR%" .. -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DBUILD_TESTS_IN_RELEASE_MODE=True %ADDITIONAL_CMAKE_ARGS% || exit /b 1
 
