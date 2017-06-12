e5c9c964541260efafefc4c34a43d1595f2e70ac
Marco Poletti <poletti.marco@gmail.com>
2017-06-12 08:02:06 +0100

    Run py.test instead of py.test-3 on OS X.

--- extras/scripts/travis_ci_install_osx.sh
+++ extras/scripts/travis_ci_install_osx.sh
@@ -47,6 +47,3 @@ pip3 install sh
 
 # This adds python-installed executables to PATH (notably py.test).
 export PATH="$(brew --prefix)/bin:$PATH"
-
-# TODO: Remove this, it's just for debugging purposes.
-ls -lah $(brew --prefix)/bin || :
