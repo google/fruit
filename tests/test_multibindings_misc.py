google/fruit

commit c563512ec5b6905a761055d60984d5c81f06b19c

Change the internal representation of Component to use a single vector instead of using separate vector for various kinds of bindings. This reduces the amount of allocation/deallocation done when moving/copying Components. However this changes the order in which multibindings are returned by the injector; that will be fixed in a follow-up commit.

include/fruit/impl/component.defn.h
@@ -20,7 +20,7 @@
 #include <fruit/component.h>
 
 #include <fruit/impl/injection_errors.h>
-#include <fruit/impl/storage/to_port/old_component_storage.h>
+#include <fruit/impl/component_storage/component_storage.h>
 
 #include <memory>
 
@@ -235,7 +235,7 @@ inline PartialComponent<fruit::impl::OldStyleInstallComponent<Component<OtherCom
 PartialComponent<Bindings...>::install(const Component<OtherCompParams...>& other_component)) {
   using Op = OpFor<fruit::impl::OldStyleInstallComponent<Component<OtherCompParams...>>>;
   (void)typename fruit::impl::meta::CheckIfError<Op>::type();
-  return {{storage, fruit::impl::OldComponentStorage(other_component.storage)}};
+  return {{storage, fruit::impl::ComponentStorage(other_component.storage)}};
 }
 
 template <typename T>

commit 589fdbcd42a5f387cbe7d654247faade43050623

Fix the order of multibindings (that was knowingly broken in commit c563512e), and add tests to make sure all corner-cases wrt lazy components are covered.

include/fruit/impl/component.defn.h
@@ -65,12 +65,12 @@ inline Component<Params...>::Component(PartialComponent<Bindings...> component)
   std::size_t num_entries = component.storage.numBindings() + Op().numEntries();
   fruit::impl::FixedSizeVector<fruit::impl::ComponentStorageEntry> entries(num_entries);
 
+  Op()(entries);
+
   component.storage.addBindings(entries);
 
   // TODO: re-enable this check somehow.
   // component.component.already_converted_to_component = true;
-
-  Op()(entries);
 
   FruitAssert(entries.size() == num_entries);
 

include/fruit/impl/component_functors.defn.h
@@ -84,8 +84,8 @@ struct Compose2ComponentFunctors {
         struct Op {
           using Result = Eval<GetResult(Op2)>;
           void operator()(FixedSizeVector<ComponentStorageEntry>& entries) {
-            Eval<Op1>()(entries);
             Eval<Op2>()(entries);
+            Eval<Op1>()(entries);
           }
           std::size_t numEntries() {
             return Eval<Op1>().numEntries() + Eval<Op2>().numEntries();
@@ -209,12 +209,12 @@ struct AddInterfaceMultibinding {
       using Result = Eval<R>;
       void operator()(FixedSizeVector<ComponentStorageEntry>& entries) {
         entries.push_back(
-            InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<
-                UnwrapType<AnnotatedI>>());
-        entries.push_back(
             InjectorStorage::createComponentStorageEntryForMultibinding<
                 UnwrapType<AnnotatedI>,
                 UnwrapType<AnnotatedC>>());
+        entries.push_back(
+            InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<
+                UnwrapType<AnnotatedI>>());
       };
 
       std::size_t numEntries() {
@@ -237,11 +237,11 @@ template <typename AnnotatedSignature, typename Lambda, typename AnnotatedI>
 struct PostProcessRegisterProviderHelper<AnnotatedSignature, Lambda, Type<AnnotatedI>> {
   inline void operator()(FixedSizeVector<ComponentStorageEntry>& entries) {
     entries.push_back(
-        InjectorStorage::createComponentStorageEntryForProvider<
-            AnnotatedSignature, Lambda>());
-    entries.push_back(
         InjectorStorage::createComponentStorageEntryForCompressedProvider<
             AnnotatedSignature, Lambda, AnnotatedI>());
+    entries.push_back(
+        InjectorStorage::createComponentStorageEntryForProvider<
+            AnnotatedSignature, Lambda>());
   }
 
   std::size_t numEntries() {
@@ -334,12 +334,12 @@ struct RegisterMultibindingProviderWithAnnotations {
       using Result = Eval<R>;
       void operator()(FixedSizeVector<ComponentStorageEntry>& entries) {
         entries.push_back(
-            InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<
-                UnwrapType<Eval<NormalizeType(SignatureType(AnnotatedSignature))>>>());
-        entries.push_back(
             InjectorStorage::createComponentStorageEntryForMultibindingProvider<
                 UnwrapType<AnnotatedSignature>,
                 UnwrapType<Lambda>>());
+        entries.push_back(
+            InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<
+                UnwrapType<Eval<NormalizeType(SignatureType(AnnotatedSignature))>>>());
       }
       std::size_t numEntries() {
         return 2;
@@ -495,12 +495,12 @@ template <typename AnnotatedSignature, typename AnnotatedI>
 struct PostProcessRegisterConstructorHelper<AnnotatedSignature, Type<AnnotatedI>> {
   inline void operator()(FixedSizeVector<ComponentStorageEntry>& entries) {
     entries.push_back(
-        InjectorStorage::createComponentStorageEntryForConstructor<
-            AnnotatedSignature>());
-    entries.push_back(
         InjectorStorage::createComponentStorageEntryForCompressedConstructor<
             AnnotatedSignature,
             AnnotatedI>());
+    entries.push_back(
+        InjectorStorage::createComponentStorageEntryForConstructor<
+            AnnotatedSignature>());
   }
   std::size_t numEntries() {
     return 2;

include/fruit/impl/component_storage/component_storage.h
@@ -36,6 +36,7 @@ namespace impl {
  */
 class ComponentStorage {
 private:
+  // The entries for this component storage (potentially including lazy component), *in reverse order*.
   FixedSizeVector<ComponentStorageEntry> entries;
 
   void destroy();

include/fruit/impl/component_storage/partial_component_storage.defn.h
@@ -93,8 +93,8 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) const {
-    previous_storage.addBindings(entries);
     entries.push_back(InjectorStorage::createComponentStorageEntryForBindInstance<C, C>(instance));
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -116,9 +116,9 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) const {
-    previous_storage.addBindings(entries);
     entries.push_back(
         InjectorStorage::createComponentStorageEntryForBindInstance<fruit::Annotated<Annotation, C>, C>(instance));
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -157,11 +157,11 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) const {
-    previous_storage.addBindings(entries);
-    entries.push_back(
-        InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<C>());
     entries.push_back(
         InjectorStorage::createComponentStorageEntryForInstanceMultibinding<C, C>(instance));
+    entries.push_back(
+        InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<C>());
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -181,11 +181,11 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) const {
-    previous_storage.addBindings(entries);
-    entries.push_back(
-        InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<fruit::Annotated<Annotation, C>>());
     entries.push_back(
         InjectorStorage::createComponentStorageEntryForInstanceMultibinding<fruit::Annotated<Annotation, C>, C>(instance));
+    entries.push_back(
+        InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<fruit::Annotated<Annotation, C>>());
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -207,14 +207,14 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) const {
-    previous_storage.addBindings(entries);
-    for (C& instance : instances) {
+    for (auto i = instances.rbegin(), i_end = instances.rend(); i != i_end; ++i) {
       // TODO: consider optimizing this so that we need just 1 MULTIBINDING_VECTOR_CREATOR entry (removing the
       // assumption that each multibinding entry is always preceded by that).
+      entries.push_back(InjectorStorage::createComponentStorageEntryForInstanceMultibinding<C, C>(*i));
       entries.push_back(
           InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<C>());
-      entries.push_back(InjectorStorage::createComponentStorageEntryForInstanceMultibinding<C, C>(instance));
     }
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -236,15 +236,15 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) const {
-    previous_storage.addBindings(entries);
-    for (C& instance : instances) {
+    for (auto i = instances.rbegin(), i_end = instances.rend(); i != i_end; ++i) {
       // TODO: consider optimizing this so that we need just 1 MULTIBINDING_VECTOR_CREATOR entry (removing the
       // assumption that each multibinding entry is always preceded by that).
       entries.push_back(
-          InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<fruit::Annotated<Annotation, C>>());
+          InjectorStorage::createComponentStorageEntryForInstanceMultibinding<fruit::Annotated<Annotation, C>, C>(*i));
       entries.push_back(
-          InjectorStorage::createComponentStorageEntryForInstanceMultibinding<fruit::Annotated<Annotation, C>, C>(instance));
+          InjectorStorage::createComponentStorageEntryForMultibindingVectorCreator<fruit::Annotated<Annotation, C>>());
     }
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -323,12 +323,12 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) {
-    previous_storage.addBindings(entries);
-
+    // Note that the entries in `other_entries` are already reversed, so we don't need to reverse them here.
     FixedSizeVector<ComponentStorageEntry> other_entries = std::move(installed_component_storage).release();
     for (ComponentStorageEntry& entry : other_entries) {
       entries.push_back(std::move(entry));
     }
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -351,8 +351,8 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) {
-    previous_storage.addBindings(entries);
     entries.push_back(ComponentStorageEntry::LazyComponentWithNoArgs::create(fun));
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {
@@ -378,8 +378,8 @@ public:
   }
 
   void addBindings(FixedSizeVector<ComponentStorageEntry>& entries) {
-    previous_storage.addBindings(entries);
     entries.push_back(ComponentStorageEntry::LazyComponentWithArgs::create(fun, std::move(args_tuple)));
+    previous_storage.addBindings(entries);
   }
 
   std::size_t numBindings() const {

include/fruit/impl/normalized_component_storage/binding_normalization.templates.h
@@ -89,6 +89,7 @@ void BindingNormalization::normalizeBindingsHelper(
 
   binding_data_map = createHashMap<TypeId, ComponentStorageEntry>();
 
+  // Unlike `entries_to_process`, this vector contains the entries in the right order.
   std::vector<ComponentStorageEntry> expanded_entries_vector;
 
   // These sets contain the lazy components whose expansion has already completed.
@@ -100,7 +101,9 @@ void BindingNormalization::normalizeBindingsHelper(
   auto components_with_no_args_with_expansion_in_progress = createLazyComponentWithNoArgsSet();
   auto components_with_args_with_expansion_in_progress = createLazyComponentWithArgsSet();
 
+  // These are in reversed order (note that toplevel_entries must also be in reverse order).
   std::vector<ComponentStorageEntry> entries_to_process(toplevel_entries.begin(), toplevel_entries.end());
+
   toplevel_entries.clear();
 
   // When we expand a lazy component, instead of removing it from the stack we change its kind (in entries_to_process)

tests/test_multibindings_misc.py
@@ -159,5 +159,383 @@ def test_multiple_various_kinds():
         COMMON_DEFINITIONS,
         source)
 
+def test_order():
+    source = '''
+        std::vector<int> numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+        // *
+        // |-- 0
+        // |-- A (non-lazy)
+        // |   |-- 1
+        // |   |-- B (non-lazy)
+        // |   |   |-- 2
+        // |   |   `-- 3
+        // |   |-- 4
+        // |   |-- C (lazy)
+        // |   |   |-- 5
+        // |   |   |-- 6
+        // |   |   |-- D (non-lazy)
+        // |   |   |   |-- 7
+        // |   |   |   |-- E (lazy)
+        // |   |   |   |   |-- 8
+        // |   |   |   |   `-- 9
+        // |   |   |   `-- 10
+        // |   |   |-- 11
+        // |   |   |-- F (lazy)
+        // |   |   |   |-- 12
+        // |   |   |   `-- 13
+        // |   |   `-- 14
+        // |   |-- 15
+        // |   |-- C (lazy, won't be expanded)
+        // |   `-- 16
+        // |-- 17
+        // |-- C (lazy, won't be expanded)
+        // `-- 18
+        
+        fruit::Component<> getRootComponent();
+        fruit::Component<> getComponentA();
+        fruit::Component<> getComponentB();
+        fruit::Component<> getComponentC();
+        fruit::Component<> getComponentD();
+        fruit::Component<> getComponentE();
+        fruit::Component<> getComponentF();
+
+        fruit::Component<> getRootComponent() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[0])
+            .install(getComponentA)
+            .addInstanceMultibinding(numbers[17])
+            .install(getComponentC)
+            .addInstanceMultibinding(numbers[18]);
+        }
+        
+        fruit::Component<> getComponentA() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[1])
+            .install(getComponentB)
+            .addInstanceMultibinding(numbers[4])
+            .install(getComponentC)
+            .addInstanceMultibinding(numbers[15])
+            .install(getComponentC)
+            .addInstanceMultibinding(numbers[16]);
+        }
+
+        fruit::Component<> getComponentB() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[2])
+            .addInstanceMultibinding(numbers[3]);
+        }
+
+        fruit::Component<> getComponentC() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[5])
+            .addInstanceMultibinding(numbers[6])
+            .install(getComponentD)
+            .addInstanceMultibinding(numbers[11])
+            .install(getComponentF)
+            .addInstanceMultibinding(numbers[14]);
+        }
+
+        fruit::Component<> getComponentD() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[7])
+            .install(getComponentE)
+            .addInstanceMultibinding(numbers[10]);
+        }
+
+        fruit::Component<> getComponentE() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[8])
+            .addInstanceMultibinding(numbers[9]);
+        }
+
+        fruit::Component<> getComponentF() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[12])
+            .addInstanceMultibinding(numbers[13]);
+        }
+
+        int main() {
+          fruit::Injector<> injector(getRootComponent());
+          std::vector<int*> result_ptrs = injector.getMultibindings<int>();
+          std::vector<int> results;
+          std::cout << "Results: ";
+          for (int* result : result_ptrs) {
+            std::cout << *result << ", ";
+            results.push_back(*result);
+          }
+          std::cout << std::endl;
+          Assert(results == numbers);
+        }
+        '''
+    expect_success(
+        COMMON_DEFINITIONS,
+        source,
+        ignore_deprecation_warnings=True)
+
+
+def test_order_with_normalized_component():
+    source = '''
+        std::vector<int> numbers = {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+            19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37};
+        // root1
+        // |-- 0
+        // |-- A (non-lazy)
+        // |   |-- 1
+        // |   |-- B (non-lazy)
+        // |   |   |-- 2
+        // |   |   `-- 3
+        // |   |-- 4
+        // |   |-- C (lazy)
+        // |   |   |-- 5
+        // |   |   |-- 6
+        // |   |   |-- D (non-lazy)
+        // |   |   |   |-- 7
+        // |   |   |   |-- E (lazy)
+        // |   |   |   |   |-- 8
+        // |   |   |   |   `-- 9
+        // |   |   |   `-- 10
+        // |   |   |-- 11
+        // |   |   |-- F (lazy)
+        // |   |   |   |-- 12
+        // |   |   |   `-- 13
+        // |   |   `-- 14
+        // |   |-- 15
+        // |   |-- C (lazy, won't be expanded)
+        // |   `-- 16
+        // |-- 17
+        // |-- C (lazy, won't be expanded)
+        // `-- 18
+        
+        // root2
+        // |-- 19
+        // |-- A2 (non-lazy)
+        // |   |-- 20
+        // |   |-- B2 (non-lazy)
+        // |   |   |-- 21
+        // |   |   `-- 22
+        // |   |-- 23
+        // |   |-- C2 (lazy)
+        // |   |   |-- 24
+        // |   |   |-- 25
+        // |   |   |-- D2 (non-lazy)
+        // |   |   |   |-- 26
+        // |   |   |   |-- E2 (lazy)
+        // |   |   |   |   |-- 27
+        // |   |   |   |   `-- 28
+        // |   |   |   `-- 29
+        // |   |   |-- 30
+        // |   |   |-- F2 (lazy)
+        // |   |   |   |-- 31
+        // |   |   |   `-- 32
+        // |   |   `-- 33
+        // |   |-- 34
+        // |   |-- C2 (lazy, won't be expanded)
+        // |   `-- 35
+        // |-- 36
+        // |-- C2 (lazy, won't be expanded)
+        // `-- 37
+
+        fruit::Component<> getRootComponent();
+        fruit::Component<> getComponentA();
+        fruit::Component<> getComponentB();
+        fruit::Component<> getComponentC();
+        fruit::Component<> getComponentD();
+        fruit::Component<> getComponentE();
+        fruit::Component<> getComponentF();
+        
+        fruit::Component<> getRootComponent2();
+        fruit::Component<> getComponentA2();
+        fruit::Component<> getComponentB2();
+        fruit::Component<> getComponentC2();
+        fruit::Component<> getComponentD2();
+        fruit::Component<> getComponentE2();
+        fruit::Component<> getComponentF2();
+
+        fruit::Component<> getRootComponent() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[0])
+            .install(getComponentA)
+            .addInstanceMultibinding(numbers[17])
+            .install(getComponentC)
+            .addInstanceMultibinding(numbers[18]);
+        }
+        
+        fruit::Component<> getComponentA() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[1])
+            .install(getComponentB)
+            .addInstanceMultibinding(numbers[4])
+            .install(getComponentC)
+            .addInstanceMultibinding(numbers[15])
+            .install(getComponentC)
+            .addInstanceMultibinding(numbers[16]);
+        }
+
+        fruit::Component<> getComponentB() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[2])
+            .addInstanceMultibinding(numbers[3]);
+        }
+
+        fruit::Component<> getComponentC() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[5])
+            .addInstanceMultibinding(numbers[6])
+            .install(getComponentD)
+            .addInstanceMultibinding(numbers[11])
+            .install(getComponentF)
+            .addInstanceMultibinding(numbers[14]);
+        }
+
+        fruit::Component<> getComponentD() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[7])
+            .install(getComponentE)
+            .addInstanceMultibinding(numbers[10]);
+        }
+
+        fruit::Component<> getComponentE() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[8])
+            .addInstanceMultibinding(numbers[9]);
+        }
+
+        fruit::Component<> getComponentF() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[12])
+            .addInstanceMultibinding(numbers[13]);
+        }
+        
+        fruit::Component<> getRootComponent2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[19])
+            .install(getComponentA2)
+            .addInstanceMultibinding(numbers[36])
+            .install(getComponentC2)
+            .addInstanceMultibinding(numbers[37]);
+        }
+        
+        fruit::Component<> getComponentA2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[20])
+            .install(getComponentB2)
+            .addInstanceMultibinding(numbers[23])
+            .install(getComponentC2)
+            .addInstanceMultibinding(numbers[34])
+            .install(getComponentC2)
+            .addInstanceMultibinding(numbers[35]);
+        }
+
+        fruit::Component<> getComponentB2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[21])
+            .addInstanceMultibinding(numbers[22]);
+        }
+
+        fruit::Component<> getComponentC2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[24])
+            .addInstanceMultibinding(numbers[25])
+            .install(getComponentD2)
+            .addInstanceMultibinding(numbers[30])
+            .install(getComponentF2)
+            .addInstanceMultibinding(numbers[33]);
+        }
+
+        fruit::Component<> getComponentD2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[26])
+            .install(getComponentE2)
+            .addInstanceMultibinding(numbers[29]);
+        }
+
+        fruit::Component<> getComponentE2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[27])
+            .addInstanceMultibinding(numbers[28]);
+        }
+
+        fruit::Component<> getComponentF2() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[31])
+            .addInstanceMultibinding(numbers[32]);
+        }        
+
+        int main() {
+          fruit::NormalizedComponent<> normalizedComponent(getRootComponent());
+          fruit::Injector<> injector(normalizedComponent, getRootComponent2());
+          std::vector<int*> result_ptrs = injector.getMultibindings<int>();
+          std::vector<int> results;
+          std::cout << "Results: ";
+          for (int* result : result_ptrs) {
+            std::cout << *result << ", ";
+            results.push_back(*result);
+          }
+          std::cout << std::endl;
+          Assert(results == numbers);
+        }
+        '''
+    expect_success(
+        COMMON_DEFINITIONS,
+        source,
+        ignore_deprecation_warnings=True)
+
+def test_with_normalized_component_lazy_components_not_deduped_across():
+    source = '''
+        std::vector<int> numbers = {0, 1, 2, 3, 4};
+        
+        // *
+        // |-- 0
+        // |-- A (lazy)
+        // |   |-- 1
+        // |   `-- 2
+        // |-- 3
+        // |-- A (lazy, won't be expanded)
+        // `-- 4
+        
+        fruit::Component<> getRootComponent();
+        fruit::Component<> getComponentA();
+        
+        fruit::Component<> getRootComponent() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[0])
+            .install(getComponentA)
+            .addInstanceMultibinding(numbers[3])
+            .install(getComponentA)
+            .addInstanceMultibinding(numbers[4]);
+        }
+        
+        fruit::Component<> getComponentA() {
+          return fruit::createComponent()
+            .addInstanceMultibinding(numbers[1])
+            .addInstanceMultibinding(numbers[2]);
+        }
+
+        int main() {
+          fruit::NormalizedComponent<> normalizedComponent(getRootComponent());
+          fruit::Injector<> injector(normalizedComponent, getRootComponent());
+          std::vector<int*> result_ptrs = injector.getMultibindings<int>();
+          std::vector<int> results;
+          std::cout << "Results: ";
+          for (int* result : result_ptrs) {
+            std::cout << *result << ", ";
+            results.push_back(*result);
+          }
+          std::cout << std::endl;
+          std::vector<int> expected_numbers = {
+            // From the NormalizedComponent
+            0, 1, 2, 3, 4,
+            // From the Component
+            0, 1, 2, 3, 4,
+          };
+          Assert(results == expected_numbers);
+        }
+        '''
+    expect_success(
+        COMMON_DEFINITIONS,
+        source)
+
+
 if __name__== '__main__':
     main(__file__)