f5f3800f54ca8c0e387195c15726779f2e3349f1
Marco Poletti <poletti.marco@gmail.com>
2018-09-13 00:33:25 +0100

    Run tests under Clang 3.9 instead of 3.7 on OS X. The 3.7 homebrew formula has been deleted (https://github.com/Homebrew/homebrew-core/pull/31762).

--- extras/scripts/travis_yml_generator.py
+++ extras/scripts/travis_yml_generator.py
@@ -162,7 +162,7 @@ add_osx_tests(compiler='gcc-6', xcode_version='8', asan=False, ubsan=False, smok
 # ld: file not found: [...]/libclang_rt.asan_osx_dynamic.dylib
 # ld: file not found: [...]/libclang_rt.ubsan_osx.a
 # Not sure if that's a limitation of Clang on OS X or just of the brew-provided binaries.
-add_osx_tests(compiler='clang-3.7', stl='libc++', asan=False, ubsan=False)
+add_osx_tests(compiler='clang-3.9', stl='libc++', asan=False, ubsan=False)
 add_osx_tests(compiler='clang-4.0', xcode_version='8', stl='libc++', smoke_tests=['DebugPlain'])
 
 # UBSan is disabled because AppleClang does not support -fsanitize=undefined.
