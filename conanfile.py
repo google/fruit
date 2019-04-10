e50eb18e8a82616cf1a1db5f01a3c23fa57ade25
tt4g <mainlinett@gmail.com>
2019-04-10 13:26:49 +0900

    Explicit include directories

--- conanfile.py
+++ conanfile.py
@@ -72,4 +72,5 @@ conan_basic_setup()''')
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
+        self.cpp_info.includedirs = ["include"]
         self.cpp_info.libs = ["fruit"]
