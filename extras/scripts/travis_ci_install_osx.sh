a1c6c7c0067a31c3a8b1d7f563ea12241ae55279
Marco Poletti <poletti.marco@gmail.com>
2019-12-22 16:40:00 -0800

    Use clang[++]-6.0 as symlink names on OS X too, to match the names under Ubuntu that the Fruit CI test script uses.

--- extras/scripts/travis_ci_install_osx.sh
+++ extras/scripts/travis_ci_install_osx.sh
@@ -28,7 +28,8 @@ gcc-9)         install_brew_package gcc@9 ;;
 clang-default) ;;
 clang-6.0)
   install_brew_package llvm@6
-  ln -s /usr/local/opt/llvm@6/bin/clang++ /usr/local/bin/clang++-6
+  ln -s /usr/local/opt/llvm@6/bin/clang++ /usr/local/bin/clang++-6.0
+  ln -s /usr/local/opt/llvm@6/bin/clang /usr/local/bin/clang-6.0
   ;;
 clang-7.0)
   install_brew_package llvm@7
