08954a9c2fc25abd04d7e52b2f3ecaed7b3db1b6
Marco Poletti <poletti.marco@gmail.com>
2020-03-29 16:16:25 -0700

    Switch back to Clang 9 for libc++ CI tests, the Clang 10 packages don't work with libc++ for some reason. Also re-enable PCHs for Clang 10 CI tests, the Clang bug requiring them to be off has now been fixed.

--- extras/scripts/travis_yml_generator.py
+++ extras/scripts/travis_yml_generator.py
@@ -151,10 +151,9 @@ add_ubuntu_tests(ubuntu_version='19.10', compiler='gcc-9', asan=False, ubsan=Fal
                  smoke_tests=['DebugPlain', 'ReleasePlain'])
 add_ubuntu_tests(ubuntu_version='19.10', compiler='clang-6.0', stl='libstdc++',
                  smoke_tests=['DebugPlain', 'DebugAsanUbsan', 'ReleasePlain'])
-add_ubuntu_tests(ubuntu_version='19.10', compiler='clang-10.0', stl='libstdc++',
-                 # Disabled due to https://bugs.llvm.org/show_bug.cgi?id=41625.
-                 use_precompiled_headers_in_tests=False)
-add_ubuntu_tests(ubuntu_version='19.10', compiler='clang-10.0', stl='libc++',
+add_ubuntu_tests(ubuntu_version='19.10', compiler='clang-10.0', stl='libstdc++')
+# Using Clang 10 + libc++ doesn't work as of March 2020, it can't find STL headers.
+add_ubuntu_tests(ubuntu_version='19.10', compiler='clang-9.0', stl='libc++',
                  # Disabled due to https://bugs.llvm.org/show_bug.cgi?id=41625.
                  use_precompiled_headers_in_tests=False)
 
