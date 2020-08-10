google/fruit

commit 94cefefb42f3685c1d64664e6aa9cbaf834b25ee

Automatically detect compiler features when running under Bazel. Before this commit, bazel builds used a hardcoded configuration, but that doesn't work e.g. on Windows where MSVC needs a different config.

BUILD
@@ -6,8 +6,9 @@ filegroup(
     name = "fruit_headers",
     srcs = glob([
         "include/**/*.h",         
-        "configuration/bazel/**/*.h",
-    ]),
+    ]) + [
+        "//third_party/fruit/configuration/bazel:fruit_config",
+    ],
 )    
 
 cc_library(
@@ -15,7 +16,7 @@ cc_library(
     srcs = glob([
         "src/*.cpp",
         "include/fruit/impl/**/*.h",
-        "configuration/bazel/**/*.h"]),
+        ]) + ["//third_party/fruit/configuration/bazel:fruit_config"],
     hdrs = glob(["include/fruit/*.h"]),
     includes = ["include", "configuration/bazel"],
     deps = [

configuration/bazel/BUILD
@@ -1,0 +1,8 @@
+load("//third_party/fruit/configuration/bazel:build_defs.bzl", "generate_fruit_config")
+
+package(default_visibility = ["//third_party/fruit:__subpackages__"])
+
+generate_fruit_config(
+  name = "fruit_config",
+  check_sources = glob(["*.cpp"])
+)

configuration/bazel/build_defs.bzl
@@ -1,0 +1,85 @@
+load("@rules_cc//cc:action_names.bzl", "C_COMPILE_ACTION_NAME")
+load("@rules_cc//cc:toolchain_utils.bzl", "find_cpp_toolchain")
+
+def _generate_fruit_config_impl(ctx):
+    cc_toolchain = find_cpp_toolchain(ctx)
+
+    feature_configuration = cc_common.configure_features(
+        ctx = ctx,
+        cc_toolchain = cc_toolchain,
+        requested_features = ctx.features,
+        unsupported_features = ctx.disabled_features,
+    )
+    c_compiler_path = cc_common.get_tool_for_action(
+        feature_configuration = feature_configuration,
+        action_name = C_COMPILE_ACTION_NAME,
+    )
+
+    check_output_files = []
+    for check_source in ctx.files.check_sources:
+        output_file = ctx.actions.declare_file(check_source.path + ".o")
+
+        c_compile_variables = cc_common.create_compile_variables(
+            feature_configuration = feature_configuration,
+            cc_toolchain = cc_toolchain,
+            user_compile_flags = ctx.fragments.cpp.copts + ctx.fragments.cpp.conlyopts,
+            source_file = check_source.path,
+            output_file = output_file.path,
+        )
+        command_line = cc_common.get_memory_inefficient_command_line(
+            feature_configuration = feature_configuration,
+            action_name = C_COMPILE_ACTION_NAME,
+            variables = c_compile_variables,
+        )
+        env = cc_common.get_environment_variables(
+            feature_configuration = feature_configuration,
+            action_name = C_COMPILE_ACTION_NAME,
+            variables = c_compile_variables,
+        )
+
+        check_name = check_source.path.split('/')[-1].split('\\')[-1]
+        check_define = 'FRUIT_HAS_%s' % check_name.upper()
+        check_output_file = ctx.actions.declare_file(check_source.path + ".h")
+
+        ctx.actions.run_shell(
+            command = '"$@" &>/dev/null && echo "#define %s 1" >"%s" || echo "#define %s 0" >"%s"; touch "%s"' % (
+                check_define, check_output_file.path, check_define, check_output_file.path, output_file.path
+            ),
+            arguments = [c_compiler_path] + command_line,
+            env = env,
+            inputs = depset(
+                [check_source],
+                transitive = [cc_toolchain.all_files],
+            ),
+            outputs = [output_file, check_output_file],
+        )
+        check_output_files.append(check_output_file)
+
+    merged_output_file = ctx.actions.declare_file(ctx.label.name + ".h")
+    ctx.actions.run_shell(
+        command = '\n'.join([
+            '(',
+            'echo "#ifndef FRUIT_CONFIG_BASE_H"',
+            'echo "#define FRUIT_CONFIG_BASE_H"',
+            'echo "#define FRUIT_USES_BOOST 1"',
+            'cat %s' % ' '.join([check_output_file.path for check_output_file in check_output_files]),
+            'echo "#endif"',
+            ')>%s' % merged_output_file.path
+        ]),
+        inputs = check_output_files,
+        outputs = [merged_output_file],
+    )
+
+    return [
+        DefaultInfo(files = depset([merged_output_file])),
+    ]
+
+generate_fruit_config = rule(
+    implementation = _generate_fruit_config_impl,
+    attrs = {
+        "check_sources": attr.label_list(allow_files = True),
+        "_cc_toolchain": attr.label(default = Label("@bazel_tools//tools/cpp:current_cc_toolchain")),
+    },
+    toolchains = ["@bazel_tools//tools/cpp:toolchain_type"],
+    fragments = ["cpp"],
+)

configuration/bazel/always_inline_attribute.cpp
@@ -1,0 +1,7 @@
+__attribute__((always_inline))
+void f() {
+}
+
+int main() {
+  return 0;
+}

configuration/bazel/attribute_deprecated.cpp
@@ -1,0 +1,5 @@
+[[deprecated]] void f();
+
+int main() {
+  return 0;
+}

configuration/bazel/builtin_unreachable.cpp
@@ -1,0 +1,13 @@
+int f() {
+  static int x = 1;
+  if (x == 1) {
+    return 0;
+  } else {
+    __builtin_unreachable();
+    // Note: the lack of return here is intentional
+  }
+}
+
+int main() {
+  return f();
+}

configuration/bazel/clang_arbitrary_overload_resolution_bug.cpp
@@ -1,0 +1,11 @@
+template <typename T, typename U>
+struct Pair {};
+
+struct Map : public Pair<int, float>, Pair<int, char> {};
+
+template <typename Value>
+Value f(Pair<int, Value>*) { return Value(); }
+
+int main() {
+  f((Map*)0);
+}

configuration/bazel/constexpr_typeid.cpp
@@ -1,0 +1,6 @@
+#include <typeinfo>
+int main() {
+  constexpr static const std::type_info& x = typeid(int);
+  (void) x;
+  return 0;
+}

commit c6d389dab4bcdb61ed1a3d9d0500f8d6feca1ecf

Fix bazel build errors introduced by 94cefefb42f3685c1d64664e6aa9cbaf834b25ee when the Fruit headers are not also installed on the system where bazel runs.

BUILD
@@ -7,7 +7,7 @@ filegroup(
     srcs = glob([
         "include/**/*.h",         
     ]) + [
-        "//third_party/fruit/configuration/bazel:fruit_config",
+        "//third_party/fruit/configuration/bazel:fruit-config-base",
     ],
 )    
 
@@ -16,11 +16,12 @@ cc_library(
     srcs = glob([
         "src/*.cpp",
         "include/fruit/impl/**/*.h",
-        ]) + ["//third_party/fruit/configuration/bazel:fruit_config"],
+        ]),
     hdrs = glob(["include/fruit/*.h"]),
     includes = ["include", "configuration/bazel"],
     deps = [
         "@boost//:unordered",
+        "//third_party/fruit/configuration/bazel:fruit-config-base",
     ],
     linkopts = ["-lm"],
 )

configuration/bazel/BUILD
@@ -3,6 +3,6 @@ load("//third_party/fruit/configuration/bazel:build_defs.bzl", "generate_fruit_c
 package(default_visibility = ["//third_party/fruit:__subpackages__"])
 
 generate_fruit_config(
-  name = "fruit_config",
+  name = "fruit-config-base",
   check_sources = glob(["*.cpp"])
 )

configuration/bazel/build_defs.bzl
@@ -17,7 +17,9 @@ def _generate_fruit_config_impl(ctx):
 
     check_output_files = []
     for check_source in ctx.files.check_sources:
-        output_file = ctx.actions.declare_file(check_source.path + ".o")
+        check_name = check_source.path[:-len(".cpp")].split('/')[-1].split('\\')[-1]
+
+        output_file = ctx.actions.declare_file(check_name + ".o")
 
         c_compile_variables = cc_common.create_compile_variables(
             feature_configuration = feature_configuration,
@@ -37,9 +39,8 @@ def _generate_fruit_config_impl(ctx):
             variables = c_compile_variables,
         )
 
-        check_name = check_source.path.split('/')[-1].split('\\')[-1]
         check_define = 'FRUIT_HAS_%s' % check_name.upper()
-        check_output_file = ctx.actions.declare_file(check_source.path + ".h")
+        check_output_file = ctx.actions.declare_file(check_name + ".h")
 
         ctx.actions.run_shell(
             command = '"$@" &>/dev/null && echo "#define %s 1" >"%s" || echo "#define %s 0" >"%s"; touch "%s"' % (
@@ -55,7 +56,7 @@ def _generate_fruit_config_impl(ctx):
         )
         check_output_files.append(check_output_file)
 
-    merged_output_file = ctx.actions.declare_file(ctx.label.name + ".h")
+    merged_output_file = ctx.actions.declare_file("fruit/impl/fruit-config-base.h")
     ctx.actions.run_shell(
         command = '\n'.join([
             '(',
@@ -70,8 +71,25 @@ def _generate_fruit_config_impl(ctx):
         outputs = [merged_output_file],
     )
 
+    compilation_context, compilation_outputs = cc_common.compile(
+        actions = ctx.actions,
+        feature_configuration = feature_configuration,
+        cc_toolchain = cc_toolchain,
+        public_hdrs = [merged_output_file],
+        name = "%s_link" % ctx.label.name,
+    )
+
+    linking_context, linking_outputs = cc_common.create_linking_context_from_compilation_outputs(
+        actions = ctx.actions,
+        feature_configuration = feature_configuration,
+        compilation_outputs = compilation_outputs,
+        cc_toolchain = cc_toolchain,
+        name = "%s_link" % ctx.label.name,
+    )
+
     return [
-        DefaultInfo(files = depset([merged_output_file])),
+        DefaultInfo(files = depset([merged_output_file]), runfiles = ctx.runfiles(files = [merged_output_file])),
+        CcInfo(compilation_context=compilation_context, linking_context=linking_context),
     ]
 
 generate_fruit_config = rule(

tests/BUILD
@@ -69,7 +69,6 @@ genrule(
     ],
     visibility = ["//third_party/fruit/tests:__subpackages__"],
     cmd = ""
-          + "FRUIT_HEADERS_LOCATION=`for f in $(locations //third_party/fruit:fruit_headers); do echo \"$$f\"; done | fgrep configuration/bazel/ | head -n 1 | sed 's|configuration/bazel/.*|./|'`;"
           + "TEST_HEADERS_LOCATION=`for f in $(locations :test_headers_filegroup); do echo \"$$f\"; done | fgrep test_macros.h | sed 's|test_macros.h|./|'`;"
           + "LIBFRUIT_LOCATION=`for f in $(locations //third_party/fruit); do echo \"$$f\"; done | fgrep libfruit.so | head -n 1 | sed 's|libfruit.so|./|'`;"
           + "LIBTEST_HEADERS_LOCATION=`for f in $(locations //third_party/fruit/tests:test_headers); do echo \"$$f\"; done | fgrep libtest_headers.so | head -n 1 | sed 's|libtest_headers.so|./|'`;"
@@ -86,8 +85,8 @@ genrule(
           + "PATH_TO_COMPILED_FRUIT_LIB='third_party/fruit/tests'\n"
           + "PATH_TO_COMPILED_TEST_HEADERS='third_party/fruit/tests/test_headers'\n"
           + "PATH_TO_COMPILED_TEST_HEADERS_LIB='third_party/fruit/tests/test_headers'\n"
-          + "PATH_TO_FRUIT_STATIC_HEADERS='$${FRUIT_HEADERS_LOCATION}/include'\n"
-          + "PATH_TO_FRUIT_GENERATED_HEADERS='$${FRUIT_HEADERS_LOCATION}/configuration/bazel'\n"
+          + "PATH_TO_FRUIT_STATIC_HEADERS='third_party/fruit/include'\n"
+          + "PATH_TO_FRUIT_GENERATED_HEADERS='third_party/fruit/configuration/bazel'\n"
           + "PATH_TO_FRUIT_TEST_HEADERS='$${TEST_HEADERS_LOCATION}'\n"
           + "ADDITIONAL_LINKER_FLAGS='-lstdc++ -lm'\n"
           + "RUN_TESTS_UNDER_VALGRIND='0'\n"